21b9142f0e1d7a54a8fc6475b4c302be50bc61d4
Keith Moulton <keithm@xmos.com>
2019-10-11 13:59:03 -0400

    now using xsim trace output

--- benchmarks/xsim_bench.py
+++ benchmarks/xsim_bench.py
@@ -7,32 +7,38 @@ import subprocess
 import argparse
 import xml.etree.ElementTree as ET
 
-def is_operator(record):
-    known_operators = [
-        'fc_deepin_shallowout_lin',
-        'conv2d_deepin_deepout_relu',
-        'conv2d_shallowin_deepout_relu',
-        'maxpool2d_deep',
-        'tflite::reference_ops::FullyConnected'
-    ]
+XSIM_TRACE_FILENAME = 'xsim_trace.out'
+XSIM_TRACE_START_COLUMN = 32
 
-    for operator in known_operators:
-        #print(record, operator)
-        if record.startswith(operator):
-            return True
+KNOWN_OPERATOR_FUNCTIONS = [
+    'fc_deepin_shallowout_lin_asm',
+    'fc_deepin_shallowout_lin_c',
+    'conv2d_deepin_deepout_relu_asm',
+    'conv2d_deepin_deepout_relu_c',
+    'conv2d_shallowin_deepout_relu_asm',
+    'conv2d_shallowin_deepout_relu_c',
+    'maxpool2d_deep_asm',
+    'maxpool2d_deep_c',
+    'tflite::reference_ops::FullyConnected'
+]
 
-    return False
-
-def iter_config(config_filename):
+def load_config(config_filename):
+    config = {
+        'clock_rate': None,
+        'tiles': []
+    }
     tree = ET.parse(config_filename)
     root = tree.getroot()
 
     for node in root.findall('System/Nodes/Node'):
+        config['clock_rate'] = int(node.get('processorMhz'))
         for proc in node.findall('Processor'):
-            tile = proc.get('codeReference')
-            cores = int(proc.get('numThreads'))
-            for core in range(cores):
-                yield tile, str(core)
+            config['tiles'].append({
+                'tile': proc.get('codeReference'),
+                'cores': int(proc.get('numThreads'))
+            })
+    
+    return config
 
 def xsim_bench(args):
     xe_file = os.path.abspath(args.xe)
@@ -46,9 +52,9 @@ def xsim_bench(args):
     with open(log_filename, 'w') as log:
         # run xsim
         if args.args:
-            cmd = 'xsim --gprof --args {} {}'.format(xe_file, args.args)
+            cmd = 'xsim -t --args {} {} > {}'.format(xe_file, args.args, XSIM_TRACE_FILENAME)
         else:
-            cmd = 'xsim --gprof {}'.format(xe_file)
+            cmd = 'xsim -t {} > {}'.format(xe_file, XSIM_TRACE_FILENAME)
         print('running: {}'.format(cmd), file=log)
         xsim_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, cwd=output_dir)
         print(xsim_output.decode('utf-8'), file=log)
@@ -59,47 +65,56 @@ def xsim_bench(args):
         xobjdump_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, cwd=output_dir)
         print(xobjdump_output.decode('utf-8'), file=log)
 
-        # run xgprof
-        config_filename = os.path.join(output_dir, 'config.xml')
+        config = load_config(os.path.join(output_dir, 'config.xml'))
 
-        for tile, core in iter_config(config_filename):
-            gprof_filename = '{}_core{}.gprof'.format(tile, core)
-            if os.path.exists(os.path.join(output_dir, gprof_filename)):
-                cmd = 'xgprof --flat-profile --brief --demangle image_n0c0.elf {}'.format(gprof_filename)
-                print('running: {}'.format(cmd), file=log)
-                xgprof_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, cwd=output_dir)
-                print(xgprof_output.decode('utf-8'), file=log)
+        # process xsim trace output
+        current_operator = None
+        current_tiles = set([])
+        current_cores = set([])
+        entry_cycle_clock = 0
+        exit_cycle_clock = 0
 
-                xgprof_lines = xgprof_output.decode('utf-8').split('\n')
-                for i_line, line in enumerate(xgprof_lines):
-                    if line.startswith('Flat profile:'):
-                        break
+        with open(os.path.join(output_dir, XSIM_TRACE_FILENAME)) as fp:
+            line = fp.readline()
+            while line:
+                index_plus = line.find('+', XSIM_TRACE_START_COLUMN)
+                trace_function = line[XSIM_TRACE_START_COLUMN:index_plus].strip()
+                if trace_function in KNOWN_OPERATOR_FUNCTIONS:
+                    tile = line[5]
+                    core = line[8]
+                    index_colon = line.find(':', index_plus)
+                    index_whitespace = line.find(' ', index_colon+2)
+                    instruction = line[index_colon+2:index_whitespace].strip()
 
-                # determine duration units
-                fields = xgprof_lines[i_line+4].split()
-                if fields[4] == 'mm/call' or fields[4] == 'um/call':
-                    units = 'Microseconds'
-                elif fields[4] == 'ms/call':
-                    units = 'Milliseconds'
-                else:
-                    print(f'Unsupported units {fields[4]}', file=sys.stderr)
+                    if not current_operator:
+                        # operator function entry
+                        index_amp = line.find('@', index_whitespace)
+                        entry_cycle_clock = int(line[index_amp+1:].strip())
+                        current_operator = trace_function
+                        current_tiles.add(tile)
+                        current_cores.add(core)
+                    elif instruction == 'retsp':
+                        # operator function exit
+                        index_amp = line.find('@', index_whitespace)
+                        exit_cycle_clock = int(line[index_amp+1:].strip())
+                        duration = int((exit_cycle_clock - entry_cycle_clock) / config['clock_rate'])
+                        current_tiles.add(tile)
+                        current_cores.add(core)
 
-                template = '{:40}{:10}{:10}{:10}'
-                print_header = True
-                for xgprof_line in xgprof_lines[i_line+5:]:
-                    fields = xgprof_line.split()
-                    if fields:
-                        if len(fields) >= 7:
-                            name = fields[6].strip()
-                            if is_operator(name):
-                                if units == 'Microseconds':
-                                    duration = float(fields[4]) / 1000.0
-                                else: # must be Milliseconds
-                                    duration = float(fields[4])
-                                if print_header:
-                                    print(template.format('Operator', 'Tile', 'Core', 'Milliseconds'))
-                                    print_header = False
-                                print(template.format(name, tile, core, duration))
+                        # print report
+                        # tiles = ','.join([t for t in current_tiles])
+                        # cores = ','.join([c for c in current_cores])
+                        # print(f'{current_operator}: {duration} us    (tiles={tiles}   cores={cores}')
+                        print(f'{current_operator}: {duration} us')
+                        # reset currents
+                        current_operator = None
+                        current_tiles = set([])
+                        current_cores = set([])
+                    else:
+                        current_tiles.add(tile)
+                        current_cores.add(core)
+
+                line = fp.readline()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()

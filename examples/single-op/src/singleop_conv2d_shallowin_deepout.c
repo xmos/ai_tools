c1b04a1ae2c8e3e4f0a721a8e166a413c3e81fd1
Keith Moulton <keithm@xmos.com>
2019-10-28 09:41:30 -0400

    updates to support new biases_padded tensor

--- examples/single-op/src/singleop_conv2d_shallowin_deepout.c
+++ examples/single-op/src/singleop_conv2d_shallowin_deepout.c
@@ -7,13 +7,13 @@
 #define WORD_ALIGNED
 #endif
 
-const int8_t WORD_ALIGNED XC_conv2d_shallowin_deepout_relu_weights[1 * 5 * 16 * 8 * 4] = {XC_CONV2D_SHALLOWIN_DEEPOUT_RELU_WEIGHTS};
-const int16_t WORD_ALIGNED XC_conv2d_shallowin_deepout_relu_biases[2 * 16] = {XC_CONV2D_SHALLOWIN_DEEPOUT_RELU_BIASES};
+const int8_t WORD_ALIGNED XC_conv2d_shallowin_deepout_relu_weights[1 * 3 * 16 * 8 * 4] = {XC_CONV2D_SHALLOWIN_DEEPOUT_RELU_WEIGHTS};
+const int16_t WORD_ALIGNED XC_conv2d_shallowin_deepout_relu_biases_padded[3 * 3 * 2 * 16] = {XC_CONV2D_SHALLOWIN_DEEPOUT_RELU_BIASES_PADDED};
 const int16_t WORD_ALIGNED XC_conv2d_shallowin_deepout_relu_shift_scale[2 * 16] = {XC_CONV2D_SHALLOWIN_DEEPOUT_RELU_SHIFT_SCALE};
 const int32_t WORD_ALIGNED XC_conv2d_shallowin_deepout_relu_unpadded_shape[4] = {XC_CONV2D_SHALLOWIN_DEEPOUT_RELU_UNPADDED_SHAPE};
 
 void singleop_conv2d_shallowin_deepout(const conv2d_shallowin_deepout_input_t *XC_conv2d_shallowin_deepout_relu_input, conv2d_shallowin_deepout_identity_t *Identity_int8)
 {
 
-     conv2d_shallowin_deepout_relu(XC_conv2d_shallowin_deepout_relu_weights, (data16_t *)XC_conv2d_shallowin_deepout_relu_biases, (int8_t*)XC_conv2d_shallowin_deepout_relu_input, Identity_int8, 5, 5, 5, 5, 16, (int16_t*) &XC_conv2d_shallowin_deepout_relu_shift_scale[0], (int16_t*) &XC_conv2d_shallowin_deepout_relu_shift_scale[16]);
+     conv2d_shallowin_deepout_relu(XC_conv2d_shallowin_deepout_relu_weights, (data16_t *)XC_conv2d_shallowin_deepout_relu_biases_padded, (int8_t*)XC_conv2d_shallowin_deepout_relu_input, Identity_int8, 5, 5, 3, 3, 16, (int16_t*) &XC_conv2d_shallowin_deepout_relu_shift_scale[0], (int16_t*) &XC_conv2d_shallowin_deepout_relu_shift_scale[16]);
 }

4b4359de00da936225c5cf1eb5f04d7bdce51859
Keith Moulton <keithm@xmos.com>
2019-10-28 14:23:36 -0400

    added test for any model

---
+++ examples/any-model/compare.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# Copyright (c) 2018-2019, XMOS Ltd, All rights reserved
+
+import sys
+import argparse
+
+import numpy as np
+
+def compare(args):
+    expected = np.fromfile(args.expected_output, dtype='int8') 
+
+    computed = np.fromfile(args.computed_output, dtype='int8')
+    len_ratio = len(computed) / len(expected)
+    if len_ratio == 2:
+        computed = np.fromfile(args.computed_output, dtype='int16')
+    elif len_ratio == 4:
+        computed = np.fromfile(args.computed_output, dtype='int32')
+
+    dequantized_expected = (expected - args.expected_zero_point) * args.expected_scale
+    dequantized_computed = (computed - args.computed_zero_point) * args.computed_scale
+
+    print('expected, computed')
+    for e, c in zip(dequantized_expected, dequantized_computed):
+        print(e, c)
+
+    # computed = computed.reshape((5,5,16))
+    # print(computed)
+    # print('------------------------------------------')
+    # expected = expected.reshape((5,5,16))
+    # print(expected)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('expected_output', help='Expected output file.')
+    parser.add_argument('-a', '--expected_zero_point', required=False, type=int, default=0,
+                        help='Expected output zero point.')
+    parser.add_argument('-b', '--expected_scale', required=False, type=float, default=1.0,
+                        help='Expected output scale.')
+    parser.add_argument('computed_output', help='Computed output file.')
+    parser.add_argument('-c', '--computed_zero_point', required=False, type=int, default=0,
+                        help='Computed zero point.')
+    parser.add_argument('-d', '--computed_scale', required=False, type=float, default=1.0,
+                        help='Computed scale.')
+    args = parser.parse_args()
+
+    compare(args)
---
+++ examples/any-model/third_party/lib_nn_operator/api/nn_operator_c.h
@@ -0,0 +1,88 @@
+
+
+#ifndef NN_OPERATOR_C_H_
+#define NN_OPERATOR_C_H_
+
+#include <stdint.h>
+
+#ifdef __XC__
+extern "C" {
+#endif
+
+
+
+void nn_mat_vec_mul_s8_c(
+    const int8_t* W,
+    const int8_t* x,
+    const unsigned N_bands,
+    const unsigned N_chunks,
+    const int16_t* shr,
+    int8_t* y);
+
+
+
+
+void conv2d_deepin_deepout_relu_c(
+    const int8_t* K, 
+    const data16_t* B,
+    const int8_t* X, 
+    int8_t* Y,
+    const int32_t height, 
+    const int32_t width,
+    const int32_t K_h, 
+    const int32_t K_w,
+    const int32_t C_out, 
+    const int32_t C_in,
+    const int16_t* shifts, 
+    const int16_t* scales);
+
+
+
+void conv2d_shallowin_deepout_relu_c(
+    const int8_t* K, 
+    const data16_t* B,
+    const int8_t* X, 
+    int8_t* Y,
+    const int32_t height, 
+    const int32_t width,
+    const int32_t K_h, 
+    const int32_t K_w,
+    const int32_t C_out,
+    const int16_t* shifts, 
+    const int16_t* scales);
+
+
+
+
+void maxpool2d_deep_c(
+    const int8_t* X, 
+    int8_t* Y,
+    const int32_t height, 
+    const int32_t width,
+    const int32_t C_in);
+
+
+
+
+void fc_deepin_shallowout_lin_c(
+    const int8_t* W, 
+    const int32_t* B,
+    const int8_t* X, 
+    int16_t* Y,
+    const int32_t C_out, 
+    const int32_t C_in,
+    const uint16_t* shifts, 
+    const int16_t* scales);
+
+
+
+void argmax_16_c(
+    const int16_t* A,
+    int32_t* C,
+    const int32_t N);
+
+#ifdef __XC__
+}   //extern "C"
+#endif
+
+#endif //NN_OPERATOR_C_H_

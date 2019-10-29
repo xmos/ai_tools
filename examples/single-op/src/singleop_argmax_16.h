ae7d2ddbfe01de8fbbbd48a1a4c3c32dd929d363
Keith Moulton <keithm@xmos.com>
2019-10-29 14:08:40 -0400

    removed no longer supported single-op app.  It was replaced by test_model app

--- examples/single-op/src/singleop_argmax_16.h
+++
@@ -1,9 +0,0 @@
-#ifndef SINGLEOP_ARGMAX_16_H
-#define SINGLEOP_ARGMAX_16_H
-
-typedef int16_t argmax_16_x_int16_t[1 * 10];
-typedef int32_t argmax_16_identity_t[1];
-
-void singleop_argmax_16(const argmax_16_x_int16_t *x_int16, argmax_16_identity_t *Identity);
-
-#endif /* SINGLEOP_ARGMAX_16_H */

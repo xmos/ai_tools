a7899f76c7984f87702e2d183111db5fa4e39a1f
Laszlo Kindrat <laszlokindrat@xmos.com>
2019-12-24 13:33:40 -0500

    ReplaceShallowinDeepoutConv2DPass started, matching tested

--- python/tflite2xcore/transformation_passes.py
+++ python/tflite2xcore/transformation_passes.py
@@ -156,10 +156,13 @@ class ReplaceQuantizedWeightBiasOperatorPass(OperatorMatchingPass):
 
     def match(self, op):
         with self.using(op):
-            return (self._weights.type == TensorType.INT8
-                    and self._input.type == TensorType.INT8
-                    and self._output.type == TensorType.INT8
-                    and self._biases.type == TensorType.INT32)
+            try:
+                return (self._weights.type == TensorType.INT8
+                        and self._input.type == TensorType.INT8
+                        and self._output.type == TensorType.INT8
+                        and self._biases.type == TensorType.INT32)
+            except IndexError:  # thrown if the op doesn't have the right tensors
+                return False
 
     @property
     @abstractmethod
@@ -313,8 +316,16 @@ class ReplaceDeepoutConv2DPass(ReplaceXCOREWeightBiasOperatorPass):
         return self._op.builtin_options['padding']
 
     def match(self, op):
-        if op.operator_code.code == BuiltinOpCodes.CONV_2D:
-            return super().match(op)
+        if super().match(op):
+            with self.using(op):
+                return (self._dilation == (1, 1)
+                        and self._strides == (1, 1)
+                        and self._padding == 'SAME'
+                        and self._weights.shape[1] % 2 == 1  # kernel height is odd
+                        and self._weights.shape[2] % 2 == 1  # kernel width is odd
+                        and self._weights.shape[0] % 16 == 0)  # deepout
+
+        return False
 
     # TODO: it would be better to do this without tensorflow
     @property
@@ -384,14 +395,9 @@ class ReplaceDeepoutConv2DPass(ReplaceXCOREWeightBiasOperatorPass):
 # TODO: write (at least regression) tests for the mutator functions
 class ReplaceDeepinDeepoutConv2DPass(ReplaceDeepoutConv2DPass):
     def match(self, op):
-        if super().match(op):
+        if op.operator_code.code == BuiltinOpCodes.CONV_2D:
             with self.using(op):
-                return (self._dilation == (1, 1)
-                        and self._strides == (1, 1)
-                        and self._padding == 'SAME'
-                        and self._weights.shape[1] % 2 == 1  # kernel height is odd
-                        and self._weights.shape[2] % 2 == 1  # kernel width is odd
-                        and self._weights.shape[0] % 16 == 0  # deepout
+                return (super().match(op)
                         and self._weights.shape[3] % 32 == 0)  # deepin
 
         return False
@@ -423,6 +429,40 @@ class ReplaceDeepinDeepoutConv2DPass(ReplaceDeepoutConv2DPass):
     def mutate_output(self, op):
         # this pass should not modify the output
         pass
+
+
+# TODO: write (at least regression) tests for the mutator functions
+class ReplaceShallowinDeepoutConv2DPass(ReplaceDeepoutConv2DPass):
+    def _match_conv2d(self, op):
+        if op.operator_code.code == BuiltinOpCodes.CONV_2D:
+            with self.using(op):
+                return (super().match(op)
+                        and self._weights.shape[3] <= 4  # shallowin
+                        and self._weights.shape[2] <= 8)  # max kernel width
+
+        return False
+
+    def _match_depthwise(self, op):
+        if op.operator_code.code == BuiltinOpCodes.DEPTHWISE_CONV_2D:
+            with self.using(op):
+                return (super().match(op)
+                        and self._weights.shape[3] == 1  # depthwise only matched with single input channel
+                        and self._weights.shape[2] <= 8)  # max kernel width
+
+        return False
+
+    def match(self, op):
+        return self._match_conv2d(op) or self._match_depthwise(op)
+
+    @property
+    def new_opcode(self):
+        return OperatorCode(XCOREOpCodes.XC_conv2d_shallowin_deepout_relu)
+
+    def mutate_output(self, op):
+        raise NotImplementedError()
+
+    def mutate_weights(self, op):
+        raise NotImplementedError()
 
 
 class RemoveUnusedBuffersPass(ModelTransformationPass):

e205572d3b124ff1b5cd53b5ac2ad3f91e8f16c1
Laszlo Kindrat <laszlokindrat@xmos.com>
2020-01-08 15:28:06 -0500

    refactored and extended tests added for conv2d replacement passes

--- python/tflite2xcore/tests/test_transformation_passes/test_ReplaceShallowinDeepoutConv2DPass.py
+++ python/tflite2xcore/tests/test_transformation_passes/test_ReplaceShallowinDeepoutConv2DPass.py
@@ -1,84 +1,41 @@
 # Copyright (c) 2019, XMOS Ltd, All rights reserved
 
 import pytest
+import itertools
 
 from pytest_cases import pytest_fixture_plus, pytest_parametrize_plus, fixture_ref
 from tflite2xcore.xcore_model import XCOREModel, TensorType
 from tflite2xcore.operator_codes import OperatorCode, BuiltinOpCodes
 from tflite2xcore.transformation_passes import ReplaceShallowinDeepoutConv2DPass
-from .test_ReplaceDeepinShallowoutFullyConnectedOutputPass import (
-    matching_perceptron,
-    non_matching_shape_perceptron,
-    mlp,
-    non_matching_mlp
+
+
+# test case parameter definitions, TODO: refactor what's common here
+from .test_ReplaceDeepinDeepoutConv2DPass import (
+    MATCHING_OUTPUT_CHANNELS,
+    MATCHING_KERNEL_HEIGHT,
+    MATCHING_PADDING,
+
+    NON_MATCHING_STRIDE_W,
+    NON_MATCHING_STRIDE_H,
+    NON_MATCHING_OUTPUT_CHANNELS,
+    NON_MATCHING_KERNEL_HEIGHT,
+
+    NON_MATCHING_TENSORS
 )
 
+MATCHING_KERNEL_WIDTH = [1, 3, 5, 7]
+MATCHING_INPUT_CHANNELS = [1, 2, 3, 4]
+MATCHING_INPUT_HEIGHT = [9, 12, 20]
+MATCHING_INPUT_WIDTH = [7, 13, 17]
 
-@pytest_fixture_plus(params=[
-    (16, 1, 1, 1), (16, 3, 3, 2), (32, 5, 5, 3), (32, 3, 7, 4)
-])
-def matching(request):
-    model = XCOREModel()
-    subgraph = model.create_subgraph()
-
-    weight_shape = list(request.param)
-    input_shape = [1, 10, 10, weight_shape[-1]]
-    tin = subgraph.create_tensor('input', TensorType.INT8, shape=input_shape, isinput=True)
-    w = subgraph.create_tensor('weights', TensorType.INT8, shape=weight_shape)
-    b = subgraph.create_tensor('biases', TensorType.INT32, shape=weight_shape[:1])
-    tout = subgraph.create_tensor(
-        'output', tin.type, shape=input_shape[:-1] + weight_shape[:1], isoutput=True)
-
-    op = subgraph.create_operator(OperatorCode(BuiltinOpCodes.CONV_2D),
-                                  inputs=[tin, w, b], outputs=[tout])
-    op.builtin_options = {'padding': 'SAME',
-                          'stride_w': 1, 'stride_h': 1,
-                          'dilation_w_factor': 1, 'dilation_h_factor': 1}
-
-    return model
+NON_MATCHING_KERNEL_WIDTH = [2, 4, 6, 9]
+NON_MATCHING_INPUT_CHANNELS = [5, 8, 16, 32, 64]
 
 
-@pytest_fixture_plus(params=[
-    ('padding', 'VALID'),
-    ('stride_w', 2),
-    ('stride_h', 4),
-    ('dilation_w_factor', 2),
-    ('dilation_h_factor', 3)
-])
-def non_matching_options(matching, request):
-    op = matching.subgraphs[0].operators[0]
-    op.builtin_options[request.param[0]] = request.param[1]
-    return matching
-
-
-@pytest_fixture_plus(params=[
-    (8, 1, 1, 4), (16, 3, 3, 32), (32, 5, 6, 1), (16, 4, 7, 3), (16, 3, 9, 3)
-])
-def non_matching_shape(matching, request):
-    subgraph = matching.subgraphs[0]
-
-    weight_shape = list(request.param)
-    input_shape = [1, 10, 10, weight_shape[-1]]
-    subgraph.get_tensor('input').shape = input_shape
-    subgraph.get_tensor('weights').shape = weight_shape
-    subgraph.get_tensor('biases').shape = weight_shape[:1]
-    subgraph.get_tensor('output').shape = input_shape[:-1] + weight_shape[:1]
-
-    return matching
-
-
-@pytest_fixture_plus(params=[
-    ('input', TensorType.INT16),
-    ('input', TensorType.INT32),
-    ('weights', TensorType.INT16),
-    ('weights', TensorType.INT32),
-    ('biases', TensorType.INT8),
-    ('biases', TensorType.INT16),
-])
-def non_matching_type(matching, request):
-    subgraph = matching.subgraphs[0]
-    subgraph.get_tensor(request.param[0]).type = request.param[1]
-    return matching
+# conv2d model builder, TODO: refactor what's common here
+from .test_ReplaceDeepinDeepoutConv2DPass import (
+    build_model,
+)
 
 
 @pytest.fixture()
@@ -86,31 +43,107 @@ def trf_pass():
     return ReplaceShallowinDeepoutConv2DPass()
 
 
-@pytest_parametrize_plus('model', [
-    fixture_ref(matching),
-])
-def test_match(model, trf_pass):
+@pytest.fixture(params=MATCHING_OUTPUT_CHANNELS)
+def output_channels(request):
+    return request.param
+
+
+@pytest.fixture(params=MATCHING_KERNEL_HEIGHT)
+def kernel_height(request):
+    return request.param
+
+
+@pytest.fixture(params=MATCHING_KERNEL_WIDTH)
+def kernel_width(request):
+    return request.param
+
+
+@pytest.fixture(params=MATCHING_INPUT_CHANNELS)
+def input_channels(request):
+    return request.param
+
+
+@pytest.fixture()
+def weight_shape(output_channels, kernel_height, kernel_width, input_channels):
+    return [output_channels, kernel_height, kernel_width, input_channels]
+
+
+@pytest.fixture(params=MATCHING_INPUT_HEIGHT)
+def input_height(request):
+    return request.param
+
+
+@pytest.fixture(params=MATCHING_INPUT_WIDTH)
+def input_width(request):
+    return request.param
+
+
+@pytest.fixture()
+def input_size(input_height, input_width):
+    return [input_height, input_width]
+
+
+@pytest.fixture(params=MATCHING_PADDING)
+def padding(request):
+    return request.param
+
+
+@pytest.fixture()
+def model(weight_shape, input_size, padding):
+    return build_model(weight_shape=weight_shape, input_size=input_size, padding=padding)
+
+
+def test_matching_params(trf_pass, model):
     assert trf_pass.match(model.subgraphs[0].operators[-1])
 
 
-@pytest_parametrize_plus('model', [
-    fixture_ref(non_matching_options),
-    fixture_ref(non_matching_shape),
-    fixture_ref(non_matching_type),
-])
-def test_non_match_conv(model, trf_pass):
+@pytest.mark.parametrize('stride_w', NON_MATCHING_STRIDE_W)
+def test_non_matching_stride_w(trf_pass, model, stride_w):
+    op = model.subgraphs[0].operators[0]
+    op.builtin_options['stride_w'] = stride_w
     assert not trf_pass.match(model.subgraphs[0].operators[-1])
 
 
-@pytest_parametrize_plus('model', [
-    fixture_ref(matching_perceptron),
-    fixture_ref(non_matching_shape_perceptron),
-    fixture_ref(mlp),
-    fixture_ref(non_matching_mlp)
-])
-def test_non_match_ops(model, trf_pass):
-    for op in model.subgraphs[0].operators:
-        assert not trf_pass.match(op)
+@pytest.mark.parametrize('stride_h', NON_MATCHING_STRIDE_H)
+def test_non_matching_stride_h(trf_pass, model, stride_h):
+    op = model.subgraphs[0].operators[0]
+    op.builtin_options['stride_h'] = stride_h
+    assert not trf_pass.match(model.subgraphs[0].operators[-1])
+
+
+@pytest.mark.parametrize('output_channels', NON_MATCHING_OUTPUT_CHANNELS)
+def test_non_matching_output_channels(trf_pass, weight_shape, input_size, padding, output_channels):
+    weight_shape[0] = output_channels
+    model = build_model(weight_shape=weight_shape, input_size=input_size, padding=padding)
+    assert not trf_pass.match(model.subgraphs[0].operators[-1])
+
+
+@pytest.mark.parametrize('kernel_height', NON_MATCHING_KERNEL_HEIGHT)
+def test_non_matching_kernel_height(trf_pass, weight_shape, input_size, padding, kernel_height):
+    weight_shape[1] = kernel_height
+    model = build_model(weight_shape=weight_shape, input_size=input_size, padding=padding)
+    assert not trf_pass.match(model.subgraphs[0].operators[-1])
+
+
+@pytest.mark.parametrize('kernel_width', NON_MATCHING_KERNEL_WIDTH)
+def test_non_matching_kernel_width(trf_pass, weight_shape, input_size, padding, kernel_width):
+    weight_shape[2] = kernel_width
+    model = build_model(weight_shape=weight_shape, input_size=input_size, padding=padding)
+    assert not trf_pass.match(model.subgraphs[0].operators[-1])
+
+
+@pytest.mark.parametrize('input_channels', NON_MATCHING_INPUT_CHANNELS)
+def test_non_matching_input_channels(trf_pass, weight_shape, input_size, padding, input_channels):
+    weight_shape[3] = input_channels
+    model = build_model(weight_shape=weight_shape, input_size=input_size, padding=padding)
+    assert not trf_pass.match(model.subgraphs[0].operators[-1])
+
+
+@pytest.mark.parametrize(*NON_MATCHING_TENSORS)
+def test_non_matching_types(trf_pass, model, tensor_name, new_type):
+    subgraph = model.subgraphs[0]
+    subgraph.get_tensor(tensor_name).type = new_type
+    assert not trf_pass.match(model.subgraphs[0].operators[-1])
 
 
 if __name__ == "__main__":

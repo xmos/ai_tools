a723ff781767a84569ff5ec71cc67333ea70e080
Aaron Stewart <aaron.stewart@xmos.com>
2020-01-17 17:41:32 -0500

    Accidentally had most of the tests commented out.

--- xcore/operator_book/test/nn_operators/src/main.c
+++ xcore/operator_book/test/nn_operators/src/main.c
@@ -42,11 +42,11 @@ int main(void)
   int ret_val;
   printf("\n\n\n");
   
-//   UnityBegin("src\\test_vpu_memcpy.xc");
-//   RUN_TEST(test_vpu_memcpy_case0);
-//   RUN_TEST(test_vpu_memcpy_case1);
-//   ret_val = UnityEnd();
-//   printf("\n\n");
+  UnityBegin("src\\test_vpu_memcpy.xc");
+  RUN_TEST(test_vpu_memcpy_case0);
+  RUN_TEST(test_vpu_memcpy_case1);
+  ret_val = UnityEnd();
+  printf("\n\n");
 
   UnityBegin("src\\test_conv2d_deepin_deepout.xc");
   RUN_TEST(test_conv2d_deepin_deepout_1x1);
@@ -66,37 +66,37 @@ int main(void)
   printf("\n\n");
 
   
-//   UnityBegin("src\\test_maxpool2d_deep.xc");
-//   RUN_TEST(test_maxpool2d_deep_case1);
-//   RUN_TEST(test_maxpool2d_deep_case2);
-//   RUN_TEST(test_maxpool2d_deep_case3);
-//   RUN_TEST(test_maxpool2d_deep_case4);
-//   ret_val = UnityEnd();
-//   printf("\n\n");
+  UnityBegin("src\\test_maxpool2d_deep.xc");
+  RUN_TEST(test_maxpool2d_deep_case1);
+  RUN_TEST(test_maxpool2d_deep_case2);
+  RUN_TEST(test_maxpool2d_deep_case3);
+  RUN_TEST(test_maxpool2d_deep_case4);
+  ret_val = UnityEnd();
+  printf("\n\n");
 
   
-//   UnityBegin("src\\test_avgpool2d_deep.xc");
-//   RUN_TEST(test_avgpool2d_deep_case1);
-//   RUN_TEST(test_avgpool2d_deep_case2);
-//   ret_val = UnityEnd();
-//   printf("\n\n");
+  UnityBegin("src\\test_avgpool2d_deep.xc");
+  RUN_TEST(test_avgpool2d_deep_case1);
+  RUN_TEST(test_avgpool2d_deep_case2);
+  ret_val = UnityEnd();
+  printf("\n\n");
 
   
-//   UnityBegin("src\\test_fc_deepin_shallowout_16.xc");
-//   RUN_TEST(test_fc_deepin_shallowout_16_case1);
-//   RUN_TEST(test_fc_deepin_shallowout_16_case2);
-//   RUN_TEST(test_fc_deepin_shallowout_16_case3);
-//   ret_val = UnityEnd();
-//   printf("\n\n");
+  UnityBegin("src\\test_fc_deepin_shallowout_16.xc");
+  RUN_TEST(test_fc_deepin_shallowout_16_case1);
+  RUN_TEST(test_fc_deepin_shallowout_16_case2);
+  RUN_TEST(test_fc_deepin_shallowout_16_case3);
+  ret_val = UnityEnd();
+  printf("\n\n");
 
   
-//   UnityBegin("src\\test_fc_deepin_shallowout_8.xc");
-//   RUN_TEST(test_fc_deepin_shallowout_8_case1);
-//   RUN_TEST(test_fc_deepin_shallowout_8_case1_5);
-//   RUN_TEST(test_fc_deepin_shallowout_8_case2);
-//   RUN_TEST(test_fc_deepin_shallowout_8_case3);
-//   ret_val = UnityEnd();
-//   printf("\n\n");
+  UnityBegin("src\\test_fc_deepin_shallowout_8.xc");
+  RUN_TEST(test_fc_deepin_shallowout_8_case1);
+  RUN_TEST(test_fc_deepin_shallowout_8_case1_5);
+  RUN_TEST(test_fc_deepin_shallowout_8_case2);
+  RUN_TEST(test_fc_deepin_shallowout_8_case3);
+  ret_val = UnityEnd();
+  printf("\n\n");
 
 
   return ret_val;

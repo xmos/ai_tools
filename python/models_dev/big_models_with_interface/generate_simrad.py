0106f0d6c7558e83ca3aaf99fd97da6fdfe6bad7
Luis Mata <luismata06@gmail.com>
2020-01-21 12:01:47 -0500

    more big generators

---
+++ python/models_dev/big_models_with_interface/generate_simrad.py
@@ -0,0 +1,131 @@
+# Copyright (c) 2018-2019, XMOS Ltd, All rights reserved
+import argparse
+import logging
+from pathlib import Path
+import tensorflow as tf
+import os
+import sys
+# TODO: make sure we don't need this hack
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'interface_development')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'model_development')))
+
+import model_interface as mi
+import tflite_utils
+import model_tools as mt
+
+DEFAULT_PATH = Path(__file__).parent.joinpath('debug', 'simrad').resolve()
+DEFAULT_EPOCHS = 10
+DEFAULT_BS = 64
+DEFAULT_AUG = False
+
+
+class Simrad(mi.KerasModel):
+    def build(self):
+        super().build()
+        # Building
+        self.core_model = tf.keras.Sequential(
+            name=self.name,
+            layers=[
+                tf.keras.Input(shape=(29, 29, 1), name='input'),
+                tf.keras.layers.Conv2D(5, (5, 5), strides=2,
+                                       activation='relu', name='conv_1'),
+                tf.keras.layers.Conv2D(50, (5, 5), strides=2,
+                                       activation='relu', name='conv_2'),
+                tf.keras.layers.Flatten(name='flaten'),
+                tf.keras.layers.Dense(100, activation='relu', name='fc_1'),
+                tf.keras.layers.Dense(10, activation='softmax', name='output')
+            ])
+        # Compilation
+        self.core_model.compile(
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+            optimizer=tf.keras.optimizers.RMSprop(learning_rate=1e-3),
+            metrics=['accuracy'])
+        # Show summary
+        self.core_model.summary()
+
+    # For training
+    def prep_data(self, aug=False):
+        self.data = mt.prepare_simrad(aug)
+
+    # For exports
+    def gen_test_data(self, aug=False):
+        if not self.data:
+            self.prep_data(aug)
+        self.data['export_data'] = self.data['x_test'][:10]
+        self.data['quant'] = self.data['x_train'][:10]
+
+    def train(self, BS, EPOCHS):
+        aug = tf.keras.preprocessing.image.ImageDataGenerator(
+            rotation_range=20, zoom_range=0.15,
+            width_shift_range=0.2, height_shift_range=0.2, shear_range=0.15,
+            horizontal_flip=True, fill_mode="nearest")
+        history_simrad = self.core_model.fit_generator(
+            aug.flow(
+                self.data['x_train'], self.data['y_train'], batch_size=BS),
+            validation_data=(self.data['x_val'], self.data['y_val']),
+            steps_per_epoch=len(self.data['x_train']) // BS,
+            epochs=EPOCHS)
+
+
+def main(path=DEFAULT_PATH, train_new_model=False,
+         BS=DEFAULT_BS, EPOCHS=DEFAULT_EPOCHS,
+         AUG=DEFAULT_AUG):
+
+    simrad = Simrad('simrad', path)
+
+    if train_new_model:
+        # Build model and compile
+        simrad.build()
+        # Prepare training data
+        simrad.prep_data(AUG)
+        # Train model
+        simrad.train(BS, EPOCHS)
+        simrad.save_core_model()
+    else:
+        # Recover previous state from file system
+        simrad.load_core_model()
+    # Generate test data
+    simrad.gen_test_data(AUG)
+    # Populate converters
+    simrad.populate_converters()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        'path', nargs='?', default=DEFAULT_PATH,
+        help='Path to a directory where models and data will be saved in subdirectories.')
+    parser.add_argument(
+        '--use_gpu', action='store_true', default=False,
+        help='Use GPU for training. Might result in non-reproducible results.')
+    parser.add_argument(
+        '--train_model', action='store_true', default=False,
+        help='Train new model instead of loading pretrained tf.keras model.')
+    parser.add_argument(
+        '--batch', type=int, default=DEFAULT_BS,
+        help='Batch size.')
+    parser.add_argument(
+        '--epochs', type=int, default=DEFAULT_EPOCHS,
+        help='Number of epochs.')
+    parser.add_argument(
+        '-aug', '--augment_dataset', action='store_true', default=False,
+        help='Create a dataset with elastic transformations.')
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', default=False,
+        help='Verbose mode.')
+    args = parser.parse_args()
+
+    verbose = args.verbose
+    if verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.getLogger('tensorflow').setLevel(logging.ERROR)
+    logging.info(f"Eager execution enabled: {tf.executing_eagerly()}")
+    tflite_utils.set_gpu_usage(args.use_gpu, verbose)
+
+    main(path=args.path,
+         train_new_model=args.train_model,
+         BS=args.batch,
+         EPOCHS=args.epochs,
+         AUG=args.augment_dataset)
--- python/models_dev/model_development/model_tools.py
+++ python/models_dev/model_development/model_tools.py
@@ -171,6 +171,25 @@ def prepare_MLP(aug=False):
             'y_test': np.float32(y_test[:500]),
             'y_val': np.float32(y_val[:100])}
 
+
+# Prepare data function for Simrad
+def prepare_simrad(aug=False):
+    x_train_crop, x_test, x_val, y_train, y_test, y_val = ecc()
+    if(aug):
+        x_train_crop, y_train_crop = expand_dataset(
+            x_train_crop, y_train, 2, sigma=4.0, alpha=16.0,
+            sizex=29, sizey=29)
+    x_train, y_train = shuffle(x_train_crop, y_train_crop)
+    y_train = y_train.reshape(y_train_crop.shape[0], 10)
+    y_test = y_test.reshape(y_test.shape[0], 10)
+    return {'x_train': np.float32(x_train[:3008]),
+            'x_test': np.float32(x_test[:500]),
+            'x_val': np.float32(x_val[:100]),
+            'y_train': np.float32(y_train[:3008]),
+            'y_test': np.float32(y_test[:500]),
+            'y_val': np.float32(y_val[:100])}
+
+
 # Viz
 def sanity_check(ds, labels):
     '''

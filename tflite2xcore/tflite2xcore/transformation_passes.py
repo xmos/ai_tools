b546fe5b98278eec0efdff8546efe56d91f1148c
Laszlo Kindrat <laszlokindrat@xmos.com>
2020-01-28 11:50:59 +0000

    requantization added to FC replacement passes
    still needs testing

--- tflite2xcore/tflite2xcore/transformation_passes.py
+++ tflite2xcore/tflite2xcore/transformation_passes.py
@@ -312,6 +312,7 @@ class ReplaceXCOREWeightBiasOperatorPass(ReplaceQuantizedOperatorPass):
         return new_op
 
 
+# TODO: write (at least regression) tests for the mutator functions
 class ReplaceDeepinAnyoutFullyConnectedPass(ReplaceXCOREWeightBiasOperatorPass):
     @property
     def matching_opcode(self):
@@ -337,24 +338,8 @@ class ReplaceDeepinAnyoutFullyConnectedPass(ReplaceXCOREWeightBiasOperatorPass):
             # rename bias tensor and change quantization mode to alert users to unusual layout
             self._biases.name = f"{op.name}/biases"
             self._biases.quantization['details_type'] = 'CustomQuantization'
-
-
-# TODO: write (at least regression) tests for the mutator functions
-class ReplaceDeepinAnyoutFullyConnectedOutputPass(ReplaceDeepinAnyoutFullyConnectedPass):
-    def match(self, op):
-        if super().match(op):
-            with self.using(op):
-                return self._output in op.subgraph.outputs
-
-        return False
-
-    @property
-    def new_opcode(self):
-        return OperatorCode(XCOREOpCodes.XC_fc_deepin_anyout_final)
-
+    
     def mutate_output(self, op):
-        # NOTE: when trying to generalize this pass to non-output operators,
-        #       keep in mind that this mutation is what can affect other operators
         with self.using(op):
             self._output.type = TensorType.INT16
             self._output.name = f"{op.name}/output"
@@ -366,6 +351,24 @@ class ReplaceDeepinAnyoutFullyConnectedOutputPass(ReplaceDeepinAnyoutFullyConnec
                 'details_type': "CustomQuantization",
                 'quantized_dimension': 0
             }
+
+    @property
+    def new_opcode(self):
+        return OperatorCode(XCOREOpCodes.XC_fc_deepin_anyout)
+
+    @abstractmethod
+    def mutate(self, op):
+        # NOTE: Overload this in subclasses, and call mutate_output appropriately
+        new_op = super().mutate(op)
+
+
+class ReplaceDeepinAnyoutFullyConnectedOutputPass(ReplaceDeepinAnyoutFullyConnectedPass):
+    def match(self, op):
+        if super().match(op):
+            with self.using(op):
+                return self._output in op.subgraph.outputs
+
+        return False
 
     def mutate(self, op):
         # NOTE: the order of these mutations is strict
@@ -383,9 +386,29 @@ class ReplaceDeepinAnyoutFullyConnectedIntermediatePass(ReplaceDeepinAnyoutFully
 
         return False
 
-    @property
-    def new_opcode(self):
-        return OperatorCode(XCOREOpCodes.XC_fc_deepin_anyout_intermediate)
+    def add_requantize(self, op):
+        # rename original output tensor
+        with self.using(op):
+            self._output.name = f"{op.name}/output_requant"
+        # create intermediate tensor
+        with self.using(op):
+            new_out = op.subgraph.create_tensor(
+                f"{op.name}/intermediate", self._output.type, self._output.shape,
+                quantization=self._output.quantization
+            )
+        # create new op, insert after original op, rewire inputs/outputs
+        new_op = op.subgraph.create_operator(
+            OperatorCode(XCOREOpCodes.XC_requantize_16_to_8),
+            inputs=[intermediate], outputs=op.outputs)
+        op.outputs = [intermediate]
+        op.subgraph.insert_operator(op, new_op, after=True)
+
+    def mutate(self, op):
+        # NOTE: the order of these mutations is strict
+        new_op = super().mutate(op)
+        requantize_op = self.add_requantize(new_op)
+        self.mutate_output(new_op)
+        return new_op
 
 
 # TODO: write (at least regression) tests for this class

c51bd7845308e5e0fc1726531f9f5db942a3a5ba
Luis Mata <luismata06@gmail.com>
2020-01-30 09:40:20 -0500

    cleaned tabs

--- examples/models/single_op_models/generate_conv2d_deepin_deepout_relu.py
+++ examples/models/single_op_models/generate_conv2d_deepin_deepout_relu.py
@@ -99,65 +99,67 @@ def initializers_logic(args):
 	else:
 		weight_init = tf.constant_initializer(args.weight_const_init)
 	return weight_init, bias_init
-	
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(
-		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument(
-		'path', nargs='?', default=DEFAULT_PATH,
-		help='Path to a directory where models and data will be saved in subdirectories.')
-	parser.add_argument(
-		'-in', '--inputs', type=int, default=DEFAULT_INPUTS,
-		help='Number of input channels')
-	parser.add_argument(
-		'-out', '--outputs', type=int, default=DEFAULT_OUTPUTS,
-		help='Number of output channels')
-	parser.add_argument(
-		'-hi', '--height', type=int, default=DEFAULT_HEIGHT,
-		help='Height of input image')
-	parser.add_argument(
-		'-wi', '--width', type=int, default=DEFAULT_WIDTH,
-		help='Width of input image')
-	parser.add_argument(
-		'-kh', '--kernel_height', type=int, default=DEFAULT_KERNEL_HEIGHT,
-		help='Height of kernel')
-	parser.add_argument(
-		'-kw', '--kernel_width', type=int, default=DEFAULT_KERNEL_WIDTH,
-		help='Width of kernel')
-	parser.add_argument(
-		'-pd', '--padding', type=str, default=DEFAULT_PADDING,
-		help='Padding mode')
-	# ---------------------------------------------------------
-	parser.add_argument(
-		'--bias_const_init', type=float, default=DEFAULT_CONST,
-		help='Initialize bias with a constant'
-	)
-	parser.add_argument(
-		'--bias_unif_init', nargs='+', type=float,
-		help='Initialize bias with a random uniform distribution delimited by the range given'
-	)
-	parser.add_argument(
-		'--weight_const_init', type=float,
-		help='Initialize weights with a constant'
-	)
-	parser.add_argument(
-		'--weight_unif_init', nargs='+', type=float, default=DEFAULT_UNIFORM,
-		help='Initialize weights with a random uniform distribution delimited by the range given'
-	)
-	# ---------------------------------------------------------
-	parser.add_argument(
-		'-v', '--verbose', action='store_true', default=False,
-		help='Verbose mode.')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        'path', nargs='?', default=DEFAULT_PATH,
+        help='Path to a directory where models and data will be saved in subdirectories.')
+    parser.add_argument(
+        '-in', '--inputs', type=int, default=DEFAULT_INPUTS,
+        help='Number of input channels')
+    parser.add_argument(
+        '-out', '--outputs', type=int, default=DEFAULT_OUTPUTS,
+        help='Number of output channels')
+    parser.add_argument(
+        '-hi', '--height', type=int, default=DEFAULT_HEIGHT,
+        help='Height of input image')
+    parser.add_argument(
+        '-wi', '--width', type=int, default=DEFAULT_WIDTH,
+        help='Width of input image')
+    parser.add_argument(
+        '-kh', '--kernel_height', type=int, default=DEFAULT_KERNEL_HEIGHT,
+        help='Height of kernel')
+    parser.add_argument(
+        '-kw', '--kernel_width', type=int, default=DEFAULT_KERNEL_WIDTH,
+        help='Width of kernel')
+    parser.add_argument(
+        '-pd', '--padding', type=str, default=DEFAULT_PADDING,
+        help='Padding mode')
+    # ---------------------------------------------------------
+    parser.add_argument(
+        '--bias_const_init', type=float, default=DEFAULT_CONST,
+        help='Initialize bias with a constant'
+    )
+    parser.add_argument(
+        '--bias_unif_init', nargs='+', type=float,
+        help='Initialize bias with a random uniform distribution delimited by the range given'
+    )
+    parser.add_argument(
+        '--weight_const_init', type=float,
+        help='Initialize weights with a constant'
+    )
+    parser.add_argument(
+        '--weight_unif_init', nargs='+', type=float, default=DEFAULT_UNIFORM,
+        help='Initialize weights with a random uniform distribution delimited by the range given'
+    )
+    # ---------------------------------------------------------
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', default=False,
+        help='Verbose mode.')
+    args = parser.parse_args()
 
-	utils.set_verbosity(args.verbose)
-	utils.set_gpu_usage(False, args.verbose)
-	weight_init, bias_init = initializers_logic(args)
+    utils.set_verbosity(args.verbose)
+    utils.set_gpu_usage(False, args.verbose)
+
+
+    weight_init, bias_init = initializers_logic(args)
+
     main(path=args.path,
-		 input_channels=args.inputs, output_channels=args.outputs,
-		 K_h=args.kernel_height, K_w=args.kernel_width,
-		 height=args.height, width=args.width,
-		 padding=args.padding,
-		 bias_init=bias_init,
-		 weight_init=weight_init)
+         input_channels=args.inputs, output_channels=args.outputs,
+         K_h=args.kernel_height, K_w=args.kernel_width,
+         height=args.height, width=args.width,
+         padding=args.padding,
+         bias_init=bias_init,
+         weight_init=weight_init)

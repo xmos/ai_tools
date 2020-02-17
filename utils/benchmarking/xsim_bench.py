e30baa63bac1b686c5fc5f2da45d3a50e6675e10
Keith Moulton <keithm@xmos.com>
2020-02-17 13:28:33 -0500

    dramatic code cleanup

--- utils/benchmarking/xsim_bench.py
+++ utils/benchmarking/xsim_bench.py
@@ -6,12 +6,12 @@ import os
 import re
 import subprocess
 from collections import deque, Counter
+import itertools
 import argparse
 import xml.etree.ElementTree as ET
 
 INDENT = ' ' * 2
 XSIM_TRACE_FILENAME = 'xsim_trace.out'
-XSIM_TRACE_START_COLUMN = 32
 
 KNOWN_OPERATOR_FUNCTIONS = [
     'conv2d_deepin_deepout_init',
@@ -46,6 +46,19 @@ class TraceContext():
         self.instruction_counter = Counter()
         self.total_instructions = 0
 
+    def update(self, other):
+        self.entry_cycle_clock = min(
+            other.entry_cycle_clock,
+            self.entry_cycle_clock,
+        )
+        self.exit_cycle_clock = max(
+            other.exit_cycle_clock,
+            self.exit_cycle_clock,
+        )
+        self.total_instructions += other.total_instructions
+        self.instruction_counter.update(other.instruction_counter)
+        self.children.extend(other.children)
+
     def report(self, clock_rate, base_indent=''):
         duration = int((self.exit_cycle_clock - self.entry_cycle_clock) / clock_rate)
 
@@ -64,26 +77,13 @@ class TraceContext():
         # children
         if self.children:
             lines.append(f'{base_indent}{INDENT}children')
-            # aggregate 
-            aggregate_identifiers = set([c.identifier for c in self.children])
-            for aggregate_identifier in sorted(aggregate_identifiers):
-                aggregate_context = TraceContext(self.tile, self.core, aggregate_identifier, sys.maxsize)
-                aggregate_context.exit_cycle_clock = 0
-                for child in self.children:
-                    if child.identifier == aggregate_identifier:
-                        aggregate_context.entry_cycle_clock = min(
-                            child.entry_cycle_clock,
-                            aggregate_context.entry_cycle_clock,
-                        )
-                        aggregate_context.exit_cycle_clock = max(
-                            child.exit_cycle_clock,
-                            aggregate_context.exit_cycle_clock,
-                        )
-                        aggregate_context.total_instructions += child.total_instructions
-                        aggregate_context.instruction_counter.update(child.instruction_counter)
-                        aggregate_context.children.extend(child.children)
+            # group children by identifiers
+            for _, group in itertools.groupby(self.children, key=lambda c: c.identifier):
+                first_child = list(itertools.islice(group, 0, 1))[0]
+                # now update the first child with the remainder of the group
+                (first_child.update(context) for context in itertools.islice(group, 1))
                 
-                child_report = aggregate_context.report(clock_rate, base_indent+INDENT*2)
+                child_report = first_child.report(clock_rate, base_indent+INDENT*2)
                 for line in child_report.split('\n'):
                     lines.append(f'{base_indent}{line}')
 
@@ -110,10 +110,29 @@ def load_config(config_filename):
     
     return config
 
+def parse_trace(data):
+    TRACE_START_COLUMN = 32
+    tile = data[:7]
+    core = data[8]
+    index_plus = data.find('+', TRACE_START_COLUMN)
+    identifier = data[TRACE_START_COLUMN:index_plus].strip()
+
+    index_colon = data.find(':', index_plus)
+    index_whitespace = data.find(' ', index_colon+2)
+    instruction = data[index_colon+2:index_whitespace].strip()
+
+    index_amp = data.find('@', index_whitespace)
+    cycle_clock = int(data[index_amp+1:].strip())
+
+    return tile, core, identifier, instruction, cycle_clock
+
+def trace_is_fnop(data):
+    return data[12:16] == 'FNOP'
+
 def xsim_bench(args):
     xe_file = os.path.abspath(args.xe)
     output_dir = os.path.abspath(args.output)
-    trace_functions = KNOWN_OPERATOR_FUNCTIONS + args.functions # NOTE: not a set because we do regex lookups
+    trace_functions = [re.compile(tf) for tf in KNOWN_OPERATOR_FUNCTIONS + args.functions]
     trace_instructions = set(KNOWN_INSTRUCTIONS + args.instructions)
 
     if not os.path.exists(output_dir):
@@ -124,9 +143,9 @@ def xsim_bench(args):
     with open(log_filename, 'w') as log:
         # run xsim
         if args.args:
-            cmd = 'xsim --trace --enable-fnop-tracing --args {} {} > {}'.format(xe_file, args.args, XSIM_TRACE_FILENAME)
+            cmd = 'xsim --trace --enable-fnop-tracing --args {} {} --trace-to {}'.format(xe_file, args.args, XSIM_TRACE_FILENAME)
         else:
-            cmd = 'xsim --trace --enable-fnop-tracing {} > {}'.format(xe_file, XSIM_TRACE_FILENAME)
+            cmd = 'xsim --trace --enable-fnop-tracing {} --trace-to {}'.format(xe_file, XSIM_TRACE_FILENAME)
         print('running: {}'.format(cmd), file=log)
         #FIXME: put these 2 lines back in
         # xsim_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, cwd=output_dir)
@@ -146,47 +165,37 @@ def xsim_bench(args):
         with open(os.path.join(output_dir, XSIM_TRACE_FILENAME)) as fp:
             line = fp.readline()
             while line:
-                tile = line[:7]
-                core = line[8]
-                index_plus = line.find('+', XSIM_TRACE_START_COLUMN)
-                identifier = line[XSIM_TRACE_START_COLUMN:index_plus].strip()
-
-                if any(re.search(regex, identifier) for regex in trace_functions):
-                    index_colon = line.find(':', index_plus)
-                    index_whitespace = line.find(' ', index_colon+2)
-                    instruction = line[index_colon+2:index_whitespace].strip()
-
-                    if instruction == 'entsp' or instruction == 'dualentsp':
-                        index_amp = line.find('@', index_whitespace)
-                        entry_cycle_clock = int(line[index_amp+1:].strip())
-
-                        new_context = TraceContext(tile, core, identifier, entry_cycle_clock)
-                        new_context.instruction_count = 1
-
-                        if context_stack:
-                            new_context.is_child = True
-                            context_stack[-1].children.append(new_context)
-                        
-                        context_stack.append(new_context)
-                    elif instruction == 'retsp':
-                        index_amp = line.find('@', index_whitespace)
-                        exit_cycle_clock = int(line[index_amp+1:].strip())
-
-                        context = context_stack.pop()
-                        context.instruction_count += 1
-                        context.exit_cycle_clock = exit_cycle_clock
-                        
-                        if not context.is_child:
-                            print(context.report(config['clock_rate']))
-                    else:
-                        if context_stack:
-                            context_stack[-1].total_instructions += 1
-                            if instruction in trace_instructions:
-                                context_stack[-1].instruction_counter[instruction] += 1
-                elif line[12:16] == 'FNOP':
+                if trace_is_fnop(line):
                     if context_stack:
                         if 'FNOP' in trace_instructions:
                             context_stack[-1].instruction_counter['FNOP'] += 1
+                else:
+                    tile, core, identifier, instruction, cycle_clock = parse_trace(line)
+
+                    if any(trace_function.search(identifier) for trace_function in trace_functions):
+                        if instruction == 'entsp' or instruction == 'dualentsp':
+
+                            new_context = TraceContext(tile, core, identifier, cycle_clock)
+                            new_context.instruction_count = 1
+
+                            if context_stack:
+                                new_context.is_child = True
+                                context_stack[-1].children.append(new_context)
+                            
+                            context_stack.append(new_context)
+                        elif instruction == 'retsp':
+
+                            context = context_stack.pop()
+                            context.instruction_count += 1
+                            context.exit_cycle_clock = cycle_clock
+                            
+                            if not context.is_child:
+                                print(context.report(config['clock_rate']))
+                        else:
+                            if context_stack:
+                                context_stack[-1].total_instructions += 1
+                                if instruction in trace_instructions:
+                                    context_stack[-1].instruction_counter[instruction] += 1
 
                 line = fp.readline()
 

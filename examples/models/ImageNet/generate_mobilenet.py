b768700b21ba9bfae96eca4c68de3d4230365fa6
Luis Mata <luismata06@gmail.com>
2020-03-12 12:50:15 -0400

    major reorg in Imagenet for generate_mobilenet

--- examples/models/ImageNet/generate_mobilenet.py
+++ examples/models/ImageNet/generate_mobilenet.py
@@ -2,16 +2,14 @@
 #
 # Copyright (c) 2020, XMOS Ltd, All rights reserved
 from pathlib import Path
-from tflite2xcore.model_generation import utils
-from imagenet_common import ImageNetModel, get_default_parser, run_main
+from imagenet_common import ImageNetModel, ImagenetDefaultParser
 import tensorflow as tf
-import json
 import numpy as np
 from mobilenet_builder import MobileNet
 
-DEFAULT_PATH = {
-    'mobilenet': Path(__file__).parent.joinpath('debug', 'mobilenet').resolve(),
-}
+
+DEFAULT_NAME = 'mobilenet'
+DEFAULT_PATH = Path(__file__).parent.joinpath('debug', DEFAULT_NAME).resolve()
 DEFAULT_EPOCHS = 10
 DEFAULT_BS = 64
 
@@ -20,16 +18,20 @@ class Mobilenet(ImageNetModel):
     def build(self, num_classes=100):
         self._prep_backend()
         # Base model without classifier or padding in between the depthwise convs
-        base_model = MobileNet(include_top=False, depth_multiplier=1, weights='imagenet',
+        base_model = MobileNet(include_top=False,
+                               depth_multiplier=1, weights='imagenet',
                                input_shape=(159, 159, 3), alpha=0.25)
+
         # model to which extract the weights of the last layer
-        top_weights = tf.keras.applications.MobileNet(include_top=True, depth_multiplier=1, weights='imagenet',
-                                                      input_shape=(128, 128, 3), alpha=0.25)
+        top_weights = tf.keras.applications.MobileNet(include_top=True,
+                                                      depth_multiplier=1,
+                                                      weights='imagenet',
+                                                      input_shape=(128, 128, 3),
+                                                      alpha=0.25)
         top_weights.compile(
             loss='sparse_categorical_crossentropy',
             optimizer=tf.keras.optimizers.RMSprop(),
-            metrics=['accuracy']
-        )
+            metrics=['accuracy'])
         w, b = top_weights.layers[90].get_weights()
         wt, bt = w[0, 0, :, :num_classes], b[:num_classes]
         global_avg_layer = tf.keras.layers.GlobalAveragePooling2D()
@@ -49,39 +51,14 @@ class Mobilenet(ImageNetModel):
         self.core_model.summary()
 
 
-def main(path=None, train_new_model=False,
-         batch_size=DEFAULT_BS, epochs=DEFAULT_EPOCHS,
-         use_aug=False, xcore_tuned=False, opt_classifier=False):
-
-    name = 'mobilenet'
-    kwargs = {
-        'name': name,
-        'path': path if path else DEFAULT_PATH[name],
-        'opt_classifier': opt_classifier,
-        'use_aug': use_aug
-    }
-
-    run_main(
-        model=Mobilenet(**kwargs),
-        train_new_model=train_new_model,
-        batch_size=batch_size, epochs=epochs
-    )
+def main(raw_args=None):
+    parser = ImagenetDefaultParser(defaults={
+        'path': DEFAULT_PATH,
+    })
+    args = parser.parse_args()
+    model = Mobilenet(name=DEFAULT_NAME, path=args.path)
+    model.run()
 
 
 if __name__ == "__main__":
-    parser = get_default_parser(DEFAULT_BS=DEFAULT_BS, DEFAULT_EPOCHS=DEFAULT_EPOCHS)
-    parser.add_argument(
-        '--xcore_tuned', action='store_true', default=False,
-        help='Use a variation of the model tuned for xcore.ai.')
-    args = parser.parse_args()
-
-    utils.set_verbosity(args.verbose)
-    utils.set_gpu_usage(args.use_gpu, args.verbose)
-
-    main(path=args.path,
-         train_new_model=args.train_model,
-         batch_size=args.batch,
-         epochs=args.epochs,
-         use_aug=args.augment_dataset,
-         xcore_tuned=args.xcore_tuned,
-         opt_classifier=args.classifier)
+    main()
--- examples/models/ImageNet/imagenet_common.py
+++ examples/models/ImageNet/imagenet_common.py
@@ -2,17 +2,16 @@
 
 import logging
 import argparse
+from pathlib import Path
 from tflite2xcore.model_generation import utils
 from tflite2xcore.model_generation.interface import KerasClassifier
-import tensorflow_datasets as tfds
 import tensorflow as tf
 import numpy as np
 
 
 class ImageNetModel(KerasClassifier):
-    def __init__(self, *args, use_aug=False, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._use_aug = use_aug
 
     def prep_data(self, *, simard_resize=False, padding=2):
         image_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/grace_hopper.jpg"
@@ -31,44 +30,31 @@ class ImageNetModel(KerasClassifier):
         self.data['export_data'] = self.data['x_test']
         self.data['quant'] = self.data['x_train']
 
-
-def get_default_parser(**kwargs):
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument(
-        'path', nargs='?', default=None,
-        help='Path to a directory where models and data will be saved in subdirectories.')
-    parser.add_argument(
-        '--use_gpu', action='store_true', default=False,
-        help='Use GPU for training. Might result in non-reproducible results.')
-    parser.add_argument(
-        '--train_model', action='store_true', default=False,
-        help='Train new model instead of loading pretrained tf.keras model.')
-    parser.add_argument(
-        '--classifier', action='store_true', default=False,
-        help='Apply classifier optimizations during xcore conversion.')
-    parser.add_argument(
-        '-bs', '--batch', type=int, default=kwargs['DEFAULT_BS'],
-        help='Batch size.')
-    parser.add_argument(
-        '-ep', '--epochs', type=int, default=kwargs['DEFAULT_EPOCHS'],
-        help='Number of epochs.')
-    parser.add_argument(
-        '-aug', '--augment_dataset', action='store_true', default=False,
-        help='Create a dataset with elastic transformations.')
-    parser.add_argument(
-        '-v', '--verbose', action='store_true', default=False,
-        help='Verbose mode.')
-    return parser
+    def run(self):
+        # Build model and compile
+        self.build()
+        # Prepare training data
+        self.prep_data()
+        self.save_core_model()
+        # Generate test data
+        self.gen_test_data()
+        # Populate converters
+        self.populate_converters()
 
 
-def run_main(model, *, train_new_model, epochs=None, batch_size=None):
-    # Build model and compile
-    model.build()
-    # Prepare training data
-    model.prep_data()
-    model.save_core_model()
-    # Generate test data
-    model.gen_test_data()
-    # Populate converters
-    model.populate_converters()
+class ImagenetDefaultParser(argparse.ArgumentParser):
+
+    def __init__(self, *args, defaults, **kwargs):
+        kwargs["formatter_class"] = argparse.ArgumentDefaultsHelpFormatter
+        super().__init__(*args, **kwargs)
+        self.add_argument(
+            'path', nargs='?', default=defaults['path'],
+            help='Path to a directory where models and data will be saved in subdirectories.')
+        self.add_argument(
+            '-v', '--verbose', action='store_true', default=False,
+            help='Verbose mode.')
+
+    def parse_args(self, *args, **kwargs):
+        args = super().parse_args(*args, **kwargs)
+        utils.set_verbosity(args.verbose)
+        return args

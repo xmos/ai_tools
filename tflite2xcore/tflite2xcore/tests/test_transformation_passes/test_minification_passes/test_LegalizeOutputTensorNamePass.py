06a942e44bd9af55ee997f258fc52bb7e4aec001
Laszlo Kindrat <laszlokindrat@xmos.com>
2020-04-02 12:56:44 -0400

    mutate tests added to LegalizeOutputTensorNamePass

--- tflite2xcore/tflite2xcore/tests/test_transformation_passes/test_minification_passes/test_LegalizeOutputTensorNamePass.py
+++ tflite2xcore/tflite2xcore/tests/test_transformation_passes/test_minification_passes/test_LegalizeOutputTensorNamePass.py
@@ -59,6 +59,16 @@ def model_multi_out(input_shape, num_splits):
                        tensor_type=TensorType.INT8, axis=2)
 
 
+@pytest.fixture()
+def model_multi_out_partial(input_shape, num_splits):
+    model = build_split(input_shape=input_shape, num_splits=num_splits,
+                        tensor_type=TensorType.INT8, axis=2)
+    op = model.subgraphs[0].operators[0]
+    for j, tensor in enumerate(op.outputs[1:]):
+        tensor.name = f"{op.name}/output_{j+1}"
+    return model
+
+
 #  ----------------------------------------------------------------------------
 #                               TEST FUNCTIONS
 #  ----------------------------------------------------------------------------
@@ -77,13 +87,8 @@ def test_matching_multi_out(trf_pass, model_multi_out):
     _test_matching_params(trf_pass, model_multi_out)
 
 
-def test_matching_multi_out_partial(trf_pass, model_multi_out):
-    subgraph = model_multi_out.subgraphs[0]
-    op = subgraph.operators[0]
-    for j, tensor in enumerate(op.outputs[1:]):
-        tensor.name = f"{op.name}/output_{j}"
-
-    _test_matching_params(trf_pass, model_multi_out)
+def test_matching_multi_out_partial(trf_pass, model_multi_out_partial):
+    _test_matching_params(trf_pass, model_multi_out_partial)
 
 
 def test_non_matching_simple(trf_pass, model_simple):
@@ -112,6 +117,50 @@ def test_non_matching_multi_out(trf_pass, model_multi_out):
         tensor.name = f"{op.name}/output_{j}"
 
     _test_non_matching_params(trf_pass, model_multi_out)
+
+
+def test_mutate_simple(trf_pass, model_simple):
+    # run mutating pass
+    trf_pass.run(model_simple)
+    model_simple.sanity_check()
+
+    op = model_simple.subgraphs[0].operators[-1]
+    assert op.outputs[0].name == f"{op.name}/output"
+
+
+def test_mutate_multi_op(trf_pass, model_multi_op):
+    # run mutating pass
+    trf_pass.run(model_multi_op)
+    model_multi_op.sanity_check()
+
+    for j, op in enumerate(model_multi_op.subgraphs[0].operators):
+        expected_name = f"{op.name}/output"
+        name = op.outputs[0].name
+        assert name == expected_name, f"op {j} name: expected '{expected_name}', found '{name}'"
+
+
+def test_mutate_multi_out(trf_pass, model_multi_out):
+    # run mutating pass
+    trf_pass.run(model_multi_out)
+    model_multi_out.sanity_check()
+
+    op = model_multi_out.subgraphs[0].operators[-1]
+    for j, tensor in enumerate(op.outputs):
+        expected_name = f"{op.name}/output_{j}"
+        name = tensor.name
+        assert name == expected_name, f"tensor {j} name: expected '{expected_name}', found '{name}'"
+
+
+def test_mutate_multi_out_partial(trf_pass, model_multi_out_partial):
+    # run mutating pass
+    trf_pass.run(model_multi_out_partial)
+    model_multi_out_partial.sanity_check()
+
+    op = model_multi_out_partial.subgraphs[0].operators[-1]
+    for j, tensor in enumerate(op.outputs):
+        expected_name = f"{op.name}/output_{j}"
+        name = tensor.name
+        assert name == expected_name, f"tensor {j} name: expected '{expected_name}', found '{name}'"
 
 
 if __name__ == "__main__":
--- tflite2xcore/tflite2xcore/transformation_passes/minification_passes.py
+++ tflite2xcore/tflite2xcore/transformation_passes/minification_passes.py
@@ -15,3 +15,24 @@ class LegalizeOutputTensorNamePass(OperatorMatchingPass):
                     return True
 
         return False
+
+    def __mutate_tensor_name(self, tensor, candidate_name):
+        subgraph = tensor.subgraph
+        if tensor.name != candidate_name:
+            unique_name = subgraph.make_unique_tensor_name(candidate_name)
+
+            if unique_name is not candidate_name:
+                self.logger.warning(
+                    f"candidate_name {candidate_name} is already used by "
+                    f"tensor {subgraph.tensors.index(tensor)}, "
+                    f"defaulting to {unique_name}"
+                )
+
+            tensor.name = unique_name
+
+    def mutate(self, op):
+        if len(op.outputs) == 1:
+            self.__mutate_tensor_name(op.outputs[0], f"{op.name}/output")
+        else:
+            for j, tensor in enumerate(op.outputs):
+                self.__mutate_tensor_name(tensor, f"{op.name}/output_{j}")

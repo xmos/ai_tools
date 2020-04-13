de3336be00ebd86ce85d2f565605424008375df3
Keith Moulton <kamoulton@gmail.com>
2020-04-13 16:44:08 -0400

    now uses correct macOS SDK by default

--- tflite2xcore/tflite2xcore/__init__.py
+++ tflite2xcore/tflite2xcore/__init__.py
@@ -8,7 +8,7 @@ __PARENT_DIR = Path(__file__).parent.absolute()
 if sys.platform.startswith("linux"):
     lib_path = os.path.join(__PARENT_DIR, 'libs/linux/libtflite2xcore.so.1.0.1')
 elif sys.platform == "darwin":
-    lib_path = os.path.join(__PARENT_DIR, 'libs/macos/libtflite2xcore.1.0.1.dylib')
+    lib_path = os.path.join(__PARENT_DIR, 'libs/macos/libtflite2xcore.dylib')
 else:
     lib_path = os.path.join(__PARENT_DIR, 'libs/windows/libtflite2xcore.dll')
 

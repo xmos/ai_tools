e1596c1347df7fdaa699a1356a44dd39e71dbf1a
Laszlo Kindrat <laszlokindrat@xmos.com>
2020-04-22 10:00:54 -0400

    separate tests for FC replacement and legalization

---
+++ tflite2xcore/tflite2xcore/tests/test_transformation_passes/test_fully_connected_passes/test_LegalizeXCFullyConnectedPass.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2020, XMOS Ltd, All rights reserved
+
+import pytest
+
+import numpy as np
+
+from tflite2xcore.transformation_passes import (
+    ReplaceFullyConnectedPass,
+    LegalizeXCFullyConnectedPass,
+)
+
+from .conftest import PARAMS
+
+
+#  ----------------------------------------------------------------------------
+#                               TEST FUNCTIONS
+#  ----------------------------------------------------------------------------
+
+
+def test_mutate(model):
+    # extract original parameters
+    op = model.subgraphs[0].operators[0]
+
+    weight_shape_old = op.inputs[1].shape
+    assert len(weight_shape_old) == 2
+    dim_out, dim_in = weight_shape_old
+
+    bias_shape_old = op.inputs[2].shape
+    assert len(bias_shape_old) == 1
+    assert bias_shape_old[0] == dim_out
+
+    # run replacement pass
+    ReplaceFullyConnectedPass().run(model)
+    model.sanity_check()
+    new_op = model.subgraphs[0].operators[0]
+
+    # run legalization pass
+    LegalizeXCFullyConnectedPass().run(model)
+    model.sanity_check()
+    assert len(new_op.inputs) == 3
+
+    # check weight tensors
+    weight_shape_new = new_op.inputs[1].shape
+    assert len(weight_shape_new) == 2
+    assert weight_shape_new[0] == dim_out
+    assert weight_shape_new[1] == int(np.ceil(dim_in / 4)) * 4
+
+    # check bias tensor
+    bss_shape = new_op.inputs[2].shape
+    assert len(bss_shape) == 3
+    assert bss_shape[0] == int(np.ceil(dim_out / 16))
+    assert bss_shape[1] == 5
+    assert bss_shape[2] == 16
+
+
+if __name__ == "__main__":
+    pytest.main()

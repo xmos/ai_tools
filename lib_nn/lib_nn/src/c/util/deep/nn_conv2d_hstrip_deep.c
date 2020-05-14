a99ddd30006360f1916976f4b334eeed6f17ad2f
Aaron Stewart <aaron.stewart@xmos.com>
2020-05-14 12:02:05 -0400

    Modified conv2d_shallowin() to support a configurable lower saturation bound (-128 (default) or -127)

--- lib_nn/lib_nn/src/c/util/deep/nn_conv2d_hstrip_deep.c
+++ lib_nn/lib_nn/src/c/util/deep/nn_conv2d_hstrip_deep.c
@@ -19,9 +19,11 @@
   #define NEG_SAT_VAL   (-128)
 #endif 
 
-const static int16_t vec_0x007F[VPU_INT8_ACC_PERIOD] = { 0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F };
 
+#if !CONFIG_SYMMETRIC_SATURATION_conv2d_deep
+const static int16_t vec_0x007F[VPU_INT8_ACC_PERIOD] = { 0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F,0x007F };
 const static int8_t vec_0x80[VPU_INT8_ACC_PERIOD] = { 0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80 };
+#endif
 
 WEAK_FUNC
 void nn_conv2d_hstrip_deep(
@@ -516,6 +518,168 @@ void nn_conv2d_hstrip_deep_padded(
 
 
 
+#define DO_VLMACCRS2(K_INCR)                                                 \
+    do {                                                                    \
+        switch(C_out_mod2){                                                 \
+            case 0:                                                         \
+                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
+                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
+                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
+                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
+            case 4:                                                         \
+                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
+                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
+                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
+                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
+            case 8:                                                         \
+                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
+                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
+                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
+                VLMACCR(&vpu, K_tmp); patch_K = ADDR(patch_K, K_INCR);            \
+                break;                                                      \
+            default:                                                        \
+                assert(0);                                                  \
+        }                                                                   \
+    } while(0)
+
+WEAK_FUNC
+void nn_conv2d_hstrip_tail_deep(
+        nn_image_t* Y,
+        const nn_image_t* X,
+        const nn_tensor_t* K,
+        const nn_bso_block_t* BSO,
+        const unsigned K_h,
+        const unsigned K_w,
+        const unsigned K_h_stride,
+        const channel_count_t C_in,
+        const mem_stride_t x_v_stride,
+        const mem_stride_t k_cout_stride,
+        const mem_stride_t y_h_stride,
+        const unsigned out_cols,
+        const channel_count_t C_out_tail)
+{
+    xs3_vpu vpu;
+
+    int8_t  vec_tmp1[2*XS3_VPU_VREG_WIDTH_BYTES];
+    int8_t* vec_tmp2 = ADDR(vec_tmp1, XS3_VPU_VREG_WIDTH_BYTES);
+
+    VSETC(&vpu, MODE_S8);
+    VCLRDR(&vpu);
+    VSTR(&vpu, vec_tmp1);
+
+    
+    const mem_stride_t win_h_stride = K_h_stride * C_in;
+    const unsigned C_in_groups = C_in >> VPU_INT8_EPV_LOG2;
+    const unsigned C_in_tail = C_in % VPU_INT8_EPV;
+
+    const unsigned C_out_mod1 = 2*(16-C_out_tail);
+    const unsigned C_out_mod2 = (C_out_mod1>>1)-4;
+    const unsigned write_mask = (1<<C_out_tail)-1;
+
+    for(int out_col = 0; out_col < out_cols; out_col++){
+
+        const nn_image_t* patch_X = X;
+        const nn_image_t* patch_K = K;
+
+        VLDD(&vpu, BSO->bias_hi);
+        VLDR(&vpu, BSO->bias_lo);
+
+        for(int pr = K_h; pr; pr--){
+            for(int col = K_w; col; col--){
+                for(int cig = C_in_groups; cig; cig--){
+
+                    VLDC(&vpu, patch_X);
+                    patch_X = ADDR(patch_X, VPU_INT8_EPV);
+
+                    VSTR(&vpu, vec_tmp2);
+                    VLDR(&vpu, ADDR(vec_tmp2, -C_out_mod1));
+                    VSTD(&vpu, vec_tmp2);
+                    VLDD(&vpu, ADDR(vec_tmp2, -C_out_mod1));
+
+                    const nn_image_t* K_tmp = ADDR(patch_K, 0);
+                    DO_VLMACCRS2(VPU_INT8_EPV);
+                }
+
+                if(C_in_tail){
+                    VLDC(&vpu, patch_X);
+
+                    VSTR(&vpu, vec_tmp2);
+                    VLDR(&vpu, ADDR(vec_tmp2, -C_out_mod1));
+                    VSTD(&vpu, vec_tmp2);
+                    VLDD(&vpu, ADDR(vec_tmp2, -C_out_mod1));
+
+                    VSTC(&vpu, vec_tmp2);
+                    VLDC(&vpu, ADDR(vec_tmp2, C_in_tail-32));
+
+                    const nn_image_t* K_tmp = ADDR(patch_K, (C_in_tail - 32));
+                    DO_VLMACCRS2(C_in_tail);
+
+                    patch_X = ADDR(patch_X, C_in_tail);
+                }
+            }
+
+            //patch_X currently pointing to pixel to right of patch in current row
+            //patch_K should be pointing to the right place
+            patch_X = ADDR(patch_X, x_v_stride);
+        }
+        
+        //Done accumulating for the current patch
+
+        //Set mode to 16-bit
+        VSETC(&vpu, MODE_S16);
+
+
+        //Saturate to 16-bit values
+        VLSAT(&vpu, BSO->shift1);
+
+        //Load scales into vC
+        VLDC(&vpu, BSO->scale);
+        VSTR(&vpu, vec_tmp2);
+        VCLRDR(&vpu);
+        VLMACC(&vpu, vec_tmp2);
+        VLDC(&vpu, BSO->offset_scale);
+        VLMACC(&vpu, BSO->offset);
+
+        
+#if CONFIG_SYMMETRIC_SATURATION_conv2d_deep
+
+        //Set mode back to 8-bit
+        VSETC(&vpu, MODE_S8);
+
+        //Saturate to 8-bit values
+        VLSAT(&vpu, BSO->shift2);
+
+        //Store result in Y
+        VSTRPV(&vpu, Y, write_mask);
+        
+#else
+
+        //Saturate to 8-bit values
+        VLSAT(&vpu, BSO->shift2);
+
+        VSTR(&vpu, vec_tmp2);
+        VLADD(&vpu, vec_0x007F);
+        VDEPTH1(&vpu);
+        uint32_t mask = ~vpu.vR.s32[0];
+
+        VLASHR(&vpu, vec_tmp2, -8);
+        VDEPTH8(&vpu);
+        
+        //Store result in Y
+        VSTRPV(&vpu, Y, write_mask);
+
+        //Set mode back to 8-bit
+        VSETC(&vpu, MODE_S8);
+
+#endif
+        
+        X = ADDR(X, win_h_stride);
+        Y = ADDR(Y, y_h_stride);
+    }
+}
+
+
+
 
 #define DO_VLMACCRS(K_addr, K_INCR)                                         \
     do {                                                                    \
@@ -856,168 +1020,6 @@ void nn_conv2d_hstrip_tail_deep_padded(
             int tmp = (pad_r <= K_h_stride)? pad_r : K_h_stride;
             center_cols -= tmp;
         }
-        
-        X = ADDR(X, win_h_stride);
-        Y = ADDR(Y, y_h_stride);
-    }
-}
-
-
-
-#define DO_VLMACCRS2(K_INCR)                                                 \
-    do {                                                                    \
-        switch(C_out_mod2){                                                 \
-            case 0:                                                         \
-                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
-                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
-                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
-                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
-            case 4:                                                         \
-                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
-                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
-                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
-                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
-            case 8:                                                         \
-                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
-                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
-                VLMACCR(&vpu, K_tmp); K_tmp = ADDR(K_tmp, k_cout_stride);         \
-                VLMACCR(&vpu, K_tmp); patch_K = ADDR(patch_K, K_INCR);            \
-                break;                                                      \
-            default:                                                        \
-                assert(0);                                                  \
-        }                                                                   \
-    } while(0)
-
-WEAK_FUNC
-void nn_conv2d_hstrip_tail_deep(
-        nn_image_t* Y,
-        const nn_image_t* X,
-        const nn_tensor_t* K,
-        const nn_bso_block_t* BSO,
-        const unsigned K_h,
-        const unsigned K_w,
-        const unsigned K_h_stride,
-        const channel_count_t C_in,
-        const mem_stride_t x_v_stride,
-        const mem_stride_t k_cout_stride,
-        const mem_stride_t y_h_stride,
-        const unsigned out_cols,
-        const channel_count_t C_out_tail)
-{
-    xs3_vpu vpu;
-
-    int8_t  vec_tmp1[2*XS3_VPU_VREG_WIDTH_BYTES];
-    int8_t* vec_tmp2 = ADDR(vec_tmp1, XS3_VPU_VREG_WIDTH_BYTES);
-
-    VSETC(&vpu, MODE_S8);
-    VCLRDR(&vpu);
-    VSTR(&vpu, vec_tmp1);
-
-    
-    const mem_stride_t win_h_stride = K_h_stride * C_in;
-    const unsigned C_in_groups = C_in >> VPU_INT8_EPV_LOG2;
-    const unsigned C_in_tail = C_in % VPU_INT8_EPV;
-
-    const unsigned C_out_mod1 = 2*(16-C_out_tail);
-    const unsigned C_out_mod2 = (C_out_mod1>>1)-4;
-    const unsigned write_mask = (1<<C_out_tail)-1;
-
-    for(int out_col = 0; out_col < out_cols; out_col++){
-
-        const nn_image_t* patch_X = X;
-        const nn_image_t* patch_K = K;
-
-        VLDD(&vpu, BSO->bias_hi);
-        VLDR(&vpu, BSO->bias_lo);
-
-        for(int pr = K_h; pr; pr--){
-            for(int col = K_w; col; col--){
-                for(int cig = C_in_groups; cig; cig--){
-
-                    VLDC(&vpu, patch_X);
-                    patch_X = ADDR(patch_X, VPU_INT8_EPV);
-
-                    VSTR(&vpu, vec_tmp2);
-                    VLDR(&vpu, ADDR(vec_tmp2, -C_out_mod1));
-                    VSTD(&vpu, vec_tmp2);
-                    VLDD(&vpu, ADDR(vec_tmp2, -C_out_mod1));
-
-                    const nn_image_t* K_tmp = ADDR(patch_K, 0);
-                    DO_VLMACCRS2(VPU_INT8_EPV);
-                }
-
-                if(C_in_tail){
-                    VLDC(&vpu, patch_X);
-
-                    VSTR(&vpu, vec_tmp2);
-                    VLDR(&vpu, ADDR(vec_tmp2, -C_out_mod1));
-                    VSTD(&vpu, vec_tmp2);
-                    VLDD(&vpu, ADDR(vec_tmp2, -C_out_mod1));
-
-                    VSTC(&vpu, vec_tmp2);
-                    VLDC(&vpu, ADDR(vec_tmp2, C_in_tail-32));
-
-                    const nn_image_t* K_tmp = ADDR(patch_K, (C_in_tail - 32));
-                    DO_VLMACCRS2(C_in_tail);
-
-                    patch_X = ADDR(patch_X, C_in_tail);
-                }
-            }
-
-            //patch_X currently pointing to pixel to right of patch in current row
-            //patch_K should be pointing to the right place
-            patch_X = ADDR(patch_X, x_v_stride);
-        }
-        
-        //Done accumulating for the current patch
-
-        //Set mode to 16-bit
-        VSETC(&vpu, MODE_S16);
-
-
-        //Saturate to 16-bit values
-        VLSAT(&vpu, BSO->shift1);
-
-        //Load scales into vC
-        VLDC(&vpu, BSO->scale);
-        VSTR(&vpu, vec_tmp2);
-        VCLRDR(&vpu);
-        VLMACC(&vpu, vec_tmp2);
-        VLDC(&vpu, BSO->offset_scale);
-        VLMACC(&vpu, BSO->offset);
-
-        
-#if CONFIG_SYMMETRIC_SATURATION_conv2d_deep
-
-        //Set mode back to 8-bit
-        VSETC(&vpu, MODE_S8);
-
-        //Saturate to 8-bit values
-        VLSAT(&vpu, BSO->shift2);
-
-        //Store result in Y
-        VSTRPV(&vpu, Y, write_mask);
-        
-#else
-
-        //Saturate to 8-bit values
-        VLSAT(&vpu, BSO->shift2);
-
-        VSTR(&vpu, vec_tmp2);
-        VLADD(&vpu, vec_0x007F);
-        VDEPTH1(&vpu);
-        uint32_t mask = ~vpu.vR.s32[0];
-
-        VLASHR(&vpu, vec_tmp2, -8);
-        VDEPTH8(&vpu);
-        
-        //Store result in Y
-        VSTRPV(&vpu, Y, write_mask);
-
-        //Set mode back to 8-bit
-        VSETC(&vpu, MODE_S8);
-
-#endif
         
         X = ADDR(X, win_h_stride);
         Y = ADDR(Y, y_h_stride);
--- lib_nn/lib_nn/src/c/util/depthwise/nn_conv2d_hstrip_depthwise.c
+++ lib_nn/lib_nn/src/c/util/depthwise/nn_conv2d_hstrip_depthwise.c
@@ -19,6 +19,9 @@
   #define NEG_SAT_VAL   (-128)
 #endif 
 
+
+
+
 static inline int8_t sat_s8_lcl(
     const int32_t acc32)
 {
@@ -50,6 +53,12 @@ static void vlmacc8(
         acc[k] += ((int32_t)X[k]) * W[k];
     }
 }
+
+
+
+
+
+
 
 WEAK_FUNC
 void nn_conv2d_hstrip_depthwise(
@@ -114,5 +123,140 @@ void nn_conv2d_hstrip_depthwise(
 
 
 
+
+
+
+
+WEAK_FUNC
+void nn_conv2d_hstrip_depthwise_padded(
+    int8_t* Y,
+    const int8_t* X_in, 
+    const int8_t* K_in,
+    const nn_bso_block_t* BSO,
+    const unsigned K_h,
+    const unsigned K_w,
+    const int32_t pad_t,
+    const int32_t pad_l_initial,
+    const int32_t pad_b,
+    const int32_t pad_r_initial,
+    const int32_t xk_col_stride,
+    const int32_t x_row_stride,
+    const int32_t window_hstride,
+    const int32_t y_col_stride,
+    const unsigned out_cols,
+    const unsigned chans_to_write,
+    const int8_t* zero_point_vec)
+{
+
+    int pad_l = pad_l_initial * xk_col_stride;
+    int pad_r = pad_r_initial * xk_col_stride;
+
+    int center_cols = xk_col_stride * K_w;
+    if(pad_l >= 0)  center_cols -= pad_l;
+    if(pad_r >= 0)  center_cols -= pad_r;
+
+    for(int out_col = 0; out_col < out_cols; out_col++){
+
+        const int8_t* X = X_in;
+        const int8_t* K = K_in;
+        // ADDR(X, "out col start");
+        // ADDR(Y, "out col start");
+        // ADDR(K, "out col start");
+
+        const int cur_pad_l = (pad_l > 0)? pad_l : 0;
+        const int cur_pad_r = (pad_r > 0)? pad_r : 0;
+
+        // printf("pads:  (%d, %d, %d, %d)\n", pad_t, pad_l, pad_l, pad_r);
+        // printf("cur_pads:  (%d, %d, %d, %d)\n", pad_t, cur_pad_l, cur_pad_l, cur_pad_r);
+
+        int32_t accs[VPU_INT8_VLMACC_ELMS];
+
+        for(int k = 0; k < VPU_INT8_VLMACC_ELMS; k++)
+            accs[k] = ((int32_t)BSO->bias_hi[k]) << VPU_INT8_ACC_VR_BITS;
+        
+        for(int k = 0; k < VPU_INT8_VLMACC_ELMS; k++)
+            accs[k] |= BSO->bias_lo[k];
+        
+        //THIS LOOP IS IN PADDING (above image)
+        for(int i = pad_t; i > 0; i--){
+            // printf("PAD_T??\t%d\t%d\n", pad_t, i);
+            for(int j = K_w; j > 0; j--){
+                vlmacc8(accs, zero_point_vec, K);
+                X = ADDR(X, xk_col_stride);
+                K = ADDR(K, xk_col_stride);
+            }
+            X = ADDR(X, x_row_stride);
+        }
+
+        // These rows are inside image (vertically)
+        for(int i = K_h - (pad_t + pad_b); i > 0; i--){
+
+            //THIS LOOP IS IN PADDING (left of image)
+            for(int j = cur_pad_l; j > 0; j -= xk_col_stride){
+                // printf("PAD_L??\t%d\t%d\n", cur_pad_l, j);
+                vlmacc8(accs, zero_point_vec, K);
+                X = ADDR(X, xk_col_stride);
+                K = ADDR(K, xk_col_stride);
+            }
+
+            for(int j = center_cols; j > 0; j-= xk_col_stride){
+                vlmacc8(accs, X, K);
+                X = ADDR(X, xk_col_stride);
+                K = ADDR(K, xk_col_stride);
+            }
+
+            //THIS LOOP IS IN PADDING (right of image)
+            for(int j = cur_pad_r; j > 0; j -= xk_col_stride){
+                // printf("PAD_R??\t%d\t%d\n", cur_pad_r, j);
+                vlmacc8(accs, zero_point_vec, K);
+                X = ADDR(X, xk_col_stride);
+                K = ADDR(K, xk_col_stride);
+            }
+
+            X = ADDR(X, x_row_stride);
+        }
+        
+        //THIS LOOP IS IN PADDING (below image)
+        for(int i = pad_b; i > 0; i--){
+            // printf("PAD_B??\t%d\t%d\n", pad_b, i);
+            for(int j = K_w; j > 0; j--){
+                vlmacc8(accs, zero_point_vec, K);
+                X = ADDR(X, xk_col_stride);
+                K = ADDR(K, xk_col_stride);
+            }
+            X = ADDR(X, x_row_stride);
+        }
+
+        for(int k = 0; k < chans_to_write; k++){
+            int16_t shift1  = BSO->shift1[k];
+            int16_t scale   = BSO->scale[k];
+            int16_t offset_scale = BSO->offset_scale[k];
+            int16_t offset  = BSO->offset[k];
+            int16_t shift2  = BSO->shift2[k];
+            accs[k] = vlsat_single_s16(accs[k], shift1);
+            accs[k] = accs[k] * scale;
+            accs[k] += ((int32_t)offset_scale)*offset;
+            accs[k] = vlsat_single_s8_lcl(accs[k], shift2);
+            Y[k] = (int8_t) accs[k];
+        }
+
+        if(pad_l > 0){
+            int tmp = (pad_l <= window_hstride)? pad_l : window_hstride;
+            center_cols += tmp;
+        }
+
+        pad_l -= (int) window_hstride;
+        pad_r += (int) window_hstride;
+
+        if(pad_r > 0){
+            int tmp = (pad_r <= window_hstride)? pad_r : window_hstride;
+            center_cols -= tmp;
+        }
+        
+        X_in = ADDR(X_in,window_hstride);
+        Y = ADDR(Y,y_col_stride);
+
+    }
+}
 
 

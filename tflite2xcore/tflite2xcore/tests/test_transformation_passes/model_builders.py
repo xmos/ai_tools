fceadea80a2d815ef2257060f4e5fb26d32834de
xross <ross@xmos.com>
2020-07-09 17:37:03 +0100

    Build_reshape sets builtin_options_type

--- tflite2xcore/tflite2xcore/tests/test_transformation_passes/model_builders.py
+++ tflite2xcore/tflite2xcore/tests/test_transformation_passes/model_builders.py
@@ -13,6 +13,7 @@ from tflite2xcore.xcore_schema import (
     OperatorCode,
     BuiltinOpCodes,
     XCOREOpCodes,
+    BuiltinOptions,
 )
 
 
@@ -661,7 +662,7 @@ def build_reshape(
         inputs = [tin]
 
     op = subgraph.create_operator(
-        OperatorCode(BuiltinOpCodes.RESHAPE), inputs=inputs, outputs=[tout], builtin_options = {"new_shape": output_shape}
+        OperatorCode(BuiltinOpCodes.RESHAPE), inputs=inputs, outputs=[tout], builtin_options = {"new_shape": output_shape}, builtin_options_type=BuiltinOptions.ReshapeOptions
     )
     return subgraph.model
 

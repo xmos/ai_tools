0019858bc7331b0c8f65c069af942ab84fdeb454
xross <ross@xmos.com>
2020-07-09 15:55:39 +0100

    Updated CanonicalizeReshapePass to check new_shape

--- tflite2xcore/tflite2xcore/transformation_passes/reshape_passes.py
+++ tflite2xcore/tflite2xcore/transformation_passes/reshape_passes.py
@@ -1,4 +1,3 @@
-
 # Copyright (c) 2020, XMOS Ltd, All rights reserved
 
 import numpy as np
@@ -11,11 +10,10 @@ from tflite2xcore.xcore_schema import (
     XCOREOpCodes,
 )
 from tflite2xcore.utils import WORD_SIZE
-from .transformation_passes import (
-    OperatorMatchingPass,
-)
+from .transformation_passes import OperatorMatchingPass
 from tflite2xcore.execution_planning import ChannelGroupSlicePlanner
 from tflite2xcore.xlogging import log_method_output
+
 
 class RemoveFlattenReshapePass(OperatorMatchingPass):
 
@@ -36,10 +34,10 @@ class RemoveFlattenReshapePass(OperatorMatchingPass):
             except IndexError:
                 # Input tensor for op has no producers..
                 return False
-            
+
             reshape_input_batch = 1
             if len(self._producer.inputs[0].shape) == 4:
-                reshape_input_batch = (self._producer.inputs[0].shape[0])
+                reshape_input_batch = self._producer.inputs[0].shape[0]
 
             reshape_output_batch = 1
             if len(op.inputs[0].shape) == 4:
@@ -49,10 +47,12 @@ class RemoveFlattenReshapePass(OperatorMatchingPass):
                 super().match(op)
                 and op.operator_code.code in self.MATCHING_OPCODES
                 and producer_opcode is BuiltinOpCodes.RESHAPE
-                and (np.prod(self._producer.inputs[0].shape) == np.prod(op.inputs[0].shape))
-                and (reshape_output_batch == reshape_input_batch)
+                and (
+                    np.prod(self._producer.inputs[0].shape)
+                    == np.prod(op.inputs[0].shape)
                 )
-            
+                and (reshape_output_batch == reshape_input_batch)
+            )
 
     def mutate(self, op):
         subgraph = op.subgraph
@@ -68,25 +68,37 @@ class RemoveFlattenReshapePass(OperatorMatchingPass):
         op.inputs[0] = producer.inputs[0]
         producer.inputs[0].consumers.append(op)
 
-class CanonicalizeReshapePass(OperatorMatchingPass):
 
+class CanonicalizeReshapePass(OperatorMatchingPass):
     def match(self, op):
 
         with self.using(op):
 
-            return (
-                super().match(op)
-                and op.operator_code.code is BuiltinOpCodes.RESHAPE
-                and 1 < len(op.inputs) < 3 
-                and op in op.inputs[1].consumers 
-                and op.inputs[1].is_constant == True
-            )            
+            if op.operator_code.code is BuiltinOpCodes.RESHAPE:
+
+                try:
+                    if op.builtin_options["new_shape"] != list(op.outputs[0].shape):
+                        self.logger.warning(
+                            "new_shape option to RESHAPE doesn't match output tensor shape"
+                        )
+                except KeyError:
+                    self.logger.warning(
+                        "Expected new_shape option to RESHAPE was not found"
+                    )
+
+                return (
+                    super().match(op)
+                    and 1 < len(op.inputs) < 3
+                    and op in op.inputs[1].consumers
+                    and op.inputs[1].is_constant == True
+                )
+            else:
+                return False
 
     def mutate(self, op):
         subgraph = op.subgraph
-       
-        #Remove connection between RESHAPE and input tensor[1+]
+
+        # Remove connection between RESHAPE and input tensor[1+]
         for i in op.inputs[1:]:
             i.consumers.remove(op)
         op.inputs = [op.inputs[0]]
-

3553014475d35b2e21fc110ed73702051aa3280b
Laszlo Kindrat <laszlokindrat@xmos.com>
2020-07-31 15:26:29 -0400

    added example for conv2d_1x1 integration test

--- utils/model_generation/conftest.py
+++ utils/model_generation/conftest.py
@@ -1,7 +1,16 @@
 # Copyright (c) 2020, XMOS Ltd, All rights reserved
 
+# TODO: move this file to final location
+
 import pytest
 import logging
+
+import tensorflow as tf
+
+from xcore_model_generation.model_generator import (
+    IntegrationTestModelGenerator,
+    Configuration,
+)
 
 
 def pytest_addoption(parser):
@@ -67,3 +76,58 @@ def run(request):
         pytest.skip()
 
     return generator.run
+
+
+class Conv2dTestModelGenerator(IntegrationTestModelGenerator):
+    def _set_config(self, cfg: Configuration) -> None:
+        input_channels = cfg.pop("input_channels", 32)
+        assert input_channels % 4 == 0, "# of input channels must be multiple of 4"
+        output_channels = cfg.pop("output_channels", 16)
+        assert output_channels % 4 == 0, "# of output channels must be multiple of 4"
+
+        self._config = dict(
+            K_w=cfg.pop("K_w", 3),
+            K_h=cfg.pop("K_h", 3),
+            height=cfg.pop("height", 5),
+            width=cfg.pop("width", 5),
+            input_channels=input_channels,
+            output_channels=output_channels,
+            padding=cfg.pop("padding", "same"),
+            strides=cfg.pop("strides", (1, 1)),
+            weight_init=cfg.pop(
+                "weight_init", tf.random_uniform_initializer(-1, 1)
+            ),  # TODO: fix init config parameters
+            bias_init=cfg.pop("bias_init", tf.constant_initializer(0)),
+        )
+        super()._set_config(cfg)
+
+    def build_core_model(self):
+        cfg = self._config
+        return tf.keras.Sequential(
+            layers=[
+                tf.keras.layers.Conv2D(
+                    filters=cfg["output_channels"],
+                    kernel_size=(cfg["K_h"], cfg["K_w"]),
+                    padding=cfg["padding"],
+                    strides=cfg["strides"],
+                    input_shape=(cfg["height"], cfg["width"], cfg["input_channels"]),
+                    bias_initializer=cfg["bias_init"],
+                    kernel_initializer=cfg["weight_init"],
+                )
+            ],
+        )
+
+    def build(self):
+        self._prep_backend()
+        try:
+            self._model = self.build_core_model()
+        except ValueError as e:
+            if e.args[0].startswith("Negative dimension size caused by"):
+                raise ValueError(
+                    "Negative dimension size (Hint: if using 'valid' padding "
+                    "verify that the kernel is at least the size of input image)"
+                ) from e
+            else:
+                raise
+        self._model.build()
+
---
+++ utils/model_generation/test_conv2d_1x1.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2020, XMOS Ltd, All rights reserved
+
+# TODO: move this file to final location
+
+import pytest
+
+import tensorflow as tf
+import numpy as np
+
+from conftest import Conv2dTestModelGenerator
+from xcore_model_generation.model_generator import Configuration
+
+
+class Conv2d1x1TestModelGenerator(Conv2dTestModelGenerator):
+    def _set_config(self, cfg: Configuration) -> None:
+        assert cfg.setdefault("K_h", 1) == 1, "Kernel height must be 1"
+        assert cfg.setdefault("K_w", 1) == 1, "Kernel width must be 1"
+        cfg.setdefault("input_channels", 4)
+        cfg.setdefault("output_channels", 4)
+        cfg.setdefault("height", 5)
+        cfg.setdefault("width", 5)
+        super()._set_config(cfg)
+
+
+GENERATOR = Conv2d1x1TestModelGenerator
+
+CONFIGS = {
+    "default": [
+        {},
+        {"height": 2, "width": 2},
+        {"height": 3, "width": 6},
+        {"input_channels": 8, "output_channels": 12},
+        {"input_channels": 32, "output_channels": 16},
+        {"height": 8, "width": 2, "input_channels": 12, "output_channels": 64},
+        {
+            "weight_init": tf.constant_initializer(1.0),
+            "bias_init": tf.constant_initializer(1.0),
+            "input_init": tf.constant_initializer(1.0),
+        },
+        {
+            "height": 1,
+            "width": 4,
+            "weight_init": tf.random_normal_initializer(0.0, 0.1),
+            "bias_init": tf.random_normal_initializer(-0.5, 0.02),
+            "input_init": tf.random_uniform_initializer(10.0),
+        },
+        {"num_threads": 2},
+        {"num_threads": 5},
+    ],
+}
+
+
+def test_foo(run):
+    for arr, arr_ref in zip(run.outputs.xcore, run.outputs.reference):
+        assert np.max(np.abs(np.int32(arr) - np.int32(arr_ref))) <= 1
+
+
+if __name__ == "__main__":
+    pytest.main()

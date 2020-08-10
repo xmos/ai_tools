420c192a03048a2dea573df6b4180ebfc9c3ed4b
xross <ross@xmos.com>
2020-08-10 21:25:34 +0100

    Improvements to unit test for SplitPaddingFromConvPass

--- tflite2xcore/tflite2xcore/tests/test_transformation_passes/test_lce_passes/test_SplitPaddingFromConvPass.py
+++ tflite2xcore/tflite2xcore/tests/test_transformation_passes/test_lce_passes/test_SplitPaddingFromConvPass.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 
 from tflite2xcore.converter import CleanupManager
 from tflite2xcore.transformation_passes.lce_passes import SplitPaddingFromConvPass
-from tflite2xcore.xcore_schema import BuiltinOpCodes, TensorType
+from tflite2xcore.xcore_schema import BuiltinOpCodes, TensorType, Padding
 
 from ..model_builders import build_lceBconv2d
 from .conftest import (
@@ -29,6 +29,26 @@ from .conftest import (
 #  ----------------------------------------------------------------------------
 
 PARAMS = deepcopy(PARAMS)
+
+PARAMS["extended"].update(
+    {
+        "padding": [Padding.SAME],
+    }
+)
+
+PARAMS["default"].update(
+    {
+        "padding": [Padding.SAME],
+    }
+)
+
+PARAMS["smoke"].update(
+    {
+        "padding": [Padding.SAME],
+    }
+)
+
+
 
 #  ----------------------------------------------------------------------------
 #                                   FIXTURES
@@ -55,41 +75,76 @@ def model(weight_shape, input_size, padding, strides):
     )
     return model
 
+@pytest.fixture()
+def model_with_non_matching_padding(weight_shape, input_size, padding, strides):
+    model = build_lceBconv2d(
+        weight_shape=weight_shape,
+        input_size=input_size,
+        padding=Padding.VALID,
+        strides=strides,
+        post_activation_bias=False,
+        post_activation_mult=False,
+    )
+    return model
+
 def test_mutate(trf_pass, model):
 
     subgraph = model.subgraphs[0]
     assert len(subgraph.operators) == 1
 
-    in_ori, out_ori = subgraph.inputs[0], subgraph.outputs[0]
+    original_input, original_output = subgraph.inputs[0], subgraph.outputs[0]
 
+    options = subgraph.operators[0].custom_options
+    strides = strides = (options["stride_height"], options["stride_width"])
+ 
     assert (len(subgraph.operators[0].inputs)) == 2
+    assert subgraph.operators[0].custom_options['padding'] == 1
 
-    # run mutating pass
+    # Run the pass
     trf_pass.run(model)
-    #model.sanity_check()
+    model.sanity_check()
+   
+    # Clean up dangling ops/tensors
+    CleanupManager(model).run_passes()
+    model.sanity_check()
 
-    # need to clean up dangling ops/tensors
-    #CleanupManager(model).run_passes()
-    #model.sanity_check()
+    assert len(subgraph.operators) == 2
 
-    #assert len(subgraph.operators) == 2
-    #op = subgraph.operators
-    #assert len(op[0].inputs) == 1
-    #assert len(op[0].outputs) == 1
-#
-#    # check input/output tensors
-#    assert len(subgraph.inputs) == 1
-#    assert len(subgraph.outputs) == 1
-#
-#    assert in_ori is op[0].inputs[0]
-#    assert in_ori in subgraph.inputs
-#
-#    assert all((i not in subgraph.inputs) for i in op[1].inputs)
-#
-#    assert out_ori is op[1].outputs[0]
-#    assert out_ori in subgraph.outputs
-#
-#    assert all((o not in subgraph.outputs) for o in op[0].outputs)
+    pad_op = subgraph.operators[0]
+    conv_op = subgraph.operators[1]
+
+    assert len(pad_op.inputs) == 2
+    assert len(pad_op.outputs) == 1
+
+    # Check input/output tensors
+    assert len(subgraph.inputs) == 1
+    assert len(subgraph.outputs) == 1
+
+    assert original_input is pad_op.inputs[0]
+    assert original_input in subgraph.inputs
+
+    assert all((i not in subgraph.inputs) for i in conv_op.inputs)
+
+    assert original_output is conv_op.outputs[0]
+    assert original_output in subgraph.outputs
+
+    assert all((o not in subgraph.outputs) for o in pad_op.outputs)
+
+    assert conv_op.outputs[0].shape == original_output.shape    
+
+    assert conv_op.custom_options['padding'] == 2
+
+    # Check special dims of PAD output tensor is as expected 
+    paddings = pad_op.inputs[1].as_array()
+    out_height_expected = pad_op.inputs[0].shape[1] + paddings[1][0] + paddings[1][1]   
+    out_width_expected  = pad_op.inputs[0].shape[2] + paddings[2][0] + paddings[2][1]   
+    
+    pad_output_shape = pad_op.outputs[0].shape
+    assert out_height_expected == pad_output_shape[1]
+    assert out_width_expected == pad_output_shape[2]
+
+def test_non_matching_paddings(trf_pass, model_with_non_matching_padding):
+    _test_non_matching_params(trf_pass, model_with_non_matching_padding)
 
 if __name__ == "__main__":
     pytest.main()

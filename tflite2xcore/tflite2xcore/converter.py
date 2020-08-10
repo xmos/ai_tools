6fcc14b634b46794a9a54a97edf90eafc7c547d9
xross <ross@xmos.com>
2020-08-10 15:01:52 +0100

    Undo LCE related changes to converter

--- tflite2xcore/tflite2xcore/converter.py
+++ tflite2xcore/tflite2xcore/converter.py
@@ -5,7 +5,6 @@ import pathlib
 from tflite2xcore.pass_manager import PassManager
 from tflite2xcore.xcore_model import XCOREModel
 from tflite2xcore import transformation_passes as passes
-from tflite2xcore.xcore_schema import TensorType
 
 
 class CleanupManager(PassManager):
@@ -88,8 +87,6 @@ def optimize_for_xcore(
         model, keep_intermediates=bool(intermediates_path), debug=debug,
     )
 
-    # TODO should this canonicalize to more closely match builtin conv?
-    pass_mgr.register_pass(passes.CanonicalizeLceBconv2DPass())
     pass_mgr.register_pass(passes.CanonicalizeReshapePass())
     pass_mgr.register_pass(passes.RemoveFlattenReshapePass())
 
@@ -109,17 +106,7 @@ def optimize_for_xcore(
     # need to cleanup after the first round of canonicalization
     pass_mgr.register_passes(CleanupManager())
 
-    # tmp pass until larq adds explicit bsign op
-    pass_mgr.register_pass(passes.SplitBsignPass())
-
-    # TOOD rename
-    # Note, this currently only matches with BConv but going forward might like to extend to other Conv ops
-    pass_mgr.register_pass(passes.SplitPaddingFromConvPass())
-
-    pass_mgr.register_pass(
-        passes.ReplaceBconv2DPass(input_tensor_type=TensorType.INT32)
-    )
-    pass_mgr.register_pass(passes.ReplaceBconv2DPass(input_tensor_type=TensorType.INT8))
+    pass_mgr.register_pass(passes.ReplaceLceBconv2DPass(input_tensor_type=TensorType.INT8))
     pass_mgr.register_pass(passes.ReplaceReLUPass())
     pass_mgr.register_pass(passes.ReplaceReLU6Pass())
     pass_mgr.register_pass(passes.ReplaceTanhPass())
@@ -135,7 +122,6 @@ def optimize_for_xcore(
     pass_mgr.register_pass(passes.ReplaceAveragePool2D2x2Pass())
     pass_mgr.register_pass(passes.ReplaceAveragePool2DPass())
     pass_mgr.register_pass(passes.ReplaceGlobalAveragePool2DPass())
-    # TODO ReplacePadPass()
 
     pass_mgr.register_pass(passes.ReplaceFullyConnectedPass())
 

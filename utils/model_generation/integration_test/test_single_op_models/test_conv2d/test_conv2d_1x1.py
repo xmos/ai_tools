82036db495aa624e2aaed03df52ae9aaf2ce8b6e
Laszlo Kindrat <laszlokindrat@xmos.com>
2020-08-11 17:10:43 -0400

    added generated test_conv2d_1x1.yml

--- utils/model_generation/integration_test/test_single_op_models/test_conv2d/test_conv2d_1x1.py
+++ utils/model_generation/integration_test/test_single_op_models/test_conv2d/test_conv2d_1x1.py
@@ -2,6 +2,8 @@
 
 import pytest  # type: ignore
 
+from tflite2xcore.xcore_model import XCOREModel  # type: ignore # TODO: fix this
+from tflite2xcore.xcore_schema import XCOREOpCodes  # type: ignore # TODO: fix this
 from tflite2xcore._model_generation import Configuration
 
 from . import Conv2dGenericTestModelGenerator, test_output
@@ -16,39 +18,23 @@ class Conv2d1x1TestModelGenerator(Conv2dGenericTestModelGenerator):
     def _set_config(self, cfg: Configuration) -> None:
         assert cfg.setdefault("K_h", 1) == 1, "Kernel height must be 1"
         assert cfg.setdefault("K_w", 1) == 1, "Kernel width must be 1"
+        assert cfg.setdefault("strides", (1, 1)) == (1, 1), "strides must be (1, 1)"
         super()._set_config(cfg)
 
 
 GENERATOR = Conv2d1x1TestModelGenerator
 
+
 #  ----------------------------------------------------------------------------
-#                                   CONFIGS
+#                                   TESTS
 #  ----------------------------------------------------------------------------
 
-CONFIGS = {
-    "default": [
-        {},
-        {"height": 2, "width": 2},
-        {"height": 3, "width": 6},
-        {"input_channels": 8, "output_channels": 12},
-        {"input_channels": 32, "output_channels": 16},
-        {"height": 8, "width": 2, "input_channels": 12, "output_channels": 64},
-        {
-            "weight_init": ("Constant", 1.0),
-            "bias_init": ("Constant", 1.0),
-            "input_init": ("Constant", 1.0),
-        },
-        {
-            "height": 1,
-            "width": 4,
-            "weight_init": ("RandomNormal", 0.0, 0.1),
-            "bias_init": ("RandomNormal", -0.5, 0.02),
-            "input_init": ("RandomUniform", 10.0),
-        },
-        {"num_threads": 2},
-        {"num_threads": 5},
-    ],
-}
+
+def test_converted_model(xcore_model: XCOREModel) -> None:
+    operators = xcore_model.subgraphs[0].operators
+    assert len(operators) == 1
+    op = operators[0]
+    assert op.operator_code.code is XCOREOpCodes.XC_conv2d_1x1
 
 
 if __name__ == "__main__":

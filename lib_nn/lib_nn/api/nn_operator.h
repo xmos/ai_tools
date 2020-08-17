1d4591337c73a92e6e69e29fd048a6748e87cf9a
Andrew Stanford-Jason <andrew@xmos.com>
2020-08-17 10:01:57 +0100

    updated the declaration of the new interface

--- lib_nn/lib_nn/api/nn_operator.h
+++ lib_nn/lib_nn/api/nn_operator.h
@@ -656,38 +656,70 @@ void bnn_reorder_kernel_tensor(const bnn_b256_t* K_p, const bnn_b256_t* K_ref_p,
 
     
 /**  
- * @brief Execute @oper{bnn_reorder_kernel_tensor}.
+ * @brief Execute @oper{bnn_conv2d_bin_out_valid}.
  * 
- * This precomputes all the parameters to efficiently execute bnn_conv2d_bin_out_asm,
- * it populates the plan with all parameters.
- * 
- * The binary conv2d performs a 2d convolution on a rectangular (sub)section of an input 
- * tensor X with a (sub)section of a kernel K and write it to a (sub)section of an output 
- * tensor Y.
+ * This performs a binary conv2d on a rectangular sub-section of an input tensor X with 
+ * kernel K.  
  * 
  * The tensor X_p represents a tensor of (x_full_height x x_full_width x X_channels)
  * The tensor K_p represents a tensor of (k_full_height x k_full_width x X_channels)
  * The tensor Y_p represents a tensor of (y_full_height x y_full_width x Y_channels)
  * 
- * The desired dimensions of X and K should be given by the parameters x and k. 
  * 
- * @param Y_p           [out]    The input image @tensor{Y}
- * @param X_p           [in]     The input image @tensor{X}
- * @param K_p           [in]     The input kernel @tensor{K}
- * @param thresholds_p  [in]     The input thresholds @tensor{thresholds}
+ * @param Y             [out]    The output image @tensor{Y}
+ * @param X             [in]     The input image @tensor{X}
+ * @param K             [in]     The input kernel @tensor{K}
+ * @param thresholds    [in]     The input thresholds @tensor{thresholds}
  * @param x             [in]     The parameters of the X image tensor
  * @param y             [in]     The parameters of the Y image tensor
  * @param k             [in]     The parameters of the K kernel tensor.
  * @param y_loc_x       [in]     The x coordinate of where the output will start writing from
  * @param y_loc_y       [in]     The y coordinate of where the output will start writing from
- * @param y_sub_width   [in]     The x coordinate of where the kernel will start reading from
- * @param y_sub_height  [in]     The y coordinate of where the kernel will start reading from
+ * @param y_sub_width   [in]     The width of the output sub-image that will be computed
+ * @param y_sub_height  [in]     The height of the output sub-image that will be computed
+ */
+void bnn_conv2d_bin_out_valid(bnn_b32_t* Y_p,
+    const bnn_b256_t* X_p, 
+    const bnn_b256_t* K_p, 
+    const int32_t* thresholds_p,
+    const nn_image_params_t* x,
+    const nn_image_params_t* y,
+    const nn_window_params_t* k, 
+
+    const unsigned y_loc_x, const unsigned y_loc_y,
+    const unsigned y_sub_width, const unsigned y_sub_height
+);
+
+/**  
+ * @brief Execute @oper{bnn_conv2d_bin_out}.
+ * 
+ * This performs a binary conv2d on a rectangular sub-section of an input tensor X with 
+ * a sub-section of kernel K and writes it to s sub-section of tensor Y.
+ * 
+ * The tensor X_p represents a tensor of (x_full_height x x_full_width x X_channels)
+ * The tensor K_p represents a tensor of (k_full_height x k_full_width x X_channels)
+ * The tensor Y_p represents a tensor of (y_full_height x y_full_width x Y_channels)
+ * 
+ * x_sub_height and x_sub_width will be infered by the parameters of y, x, k, y_loc_x, 
+ * y_loc_y, y_sub_width, y_sub_height, k_loc_x, k_loc_y, k_sub_width and k_sub_height.
+ * 
+ * @param Y             [out]    The output image @tensor{Y}
+ * @param X             [in]     The input image @tensor{X}
+ * @param K             [in]     The input kernel @tensor{K}
+ * @param thresholds    [in]     The input thresholds @tensor{thresholds}
+ * @param x             [in]     The parameters of the X image tensor
+ * @param y             [in]     The parameters of the Y image tensor
+ * @param k             [in]     The parameters of the K kernel tensor.
+ * @param y_loc_x       [in]     The x coordinate of where the output will start writing from
+ * @param y_loc_y       [in]     The y coordinate of where the output will start writing from
+ * @param y_sub_width   [in]     The width of the output sub-image that will be computed
+ * @param y_sub_height  [in]     The height of the output sub-image that will be computed
  * @param x_loc_x       [in]     The x coordinate of where the input will start reading from
  * @param x_loc_y       [in]     The y coordinate of where the input will start reading from
  * @param k_loc_x       [in]     The x coordinate of where the kernel will start reading from
  * @param k_loc_y       [in]     The y coordinate of where the kernel will start reading from
- * @param k_sub_width   [in]     The x coordinate of where the kernel will start reading from
- * @param k_sub_height  [in]     The y coordinate of where the kernel will start reading from
+ * @param k_sub_width   [in]     The width of the input sub-kernel that will be computed
+ * @param k_sub_height  [in]     The height of the input sub-kernel that will be computed
  */
 void bnn_conv2d_bin_out(bnn_b32_t* Y_p,
     const bnn_b256_t* X_p, const bnn_b256_t* K_p, const int32_t* thresholds_p,

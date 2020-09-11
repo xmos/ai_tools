578882a879784686c12acd3e55388b112b6b2cfe
xross <ross@xmos.com>
2020-09-11 11:14:59 +0100

    Updated BNN op naming

--- tflite2xcore/tflite2xcore/serialization/xcore_schema.py
+++ tflite2xcore/tflite2xcore/serialization/xcore_schema.py
@@ -125,8 +125,8 @@ class XCOREOpCodes(CustomOpCodes, KnownOpCodes):
     XC_conv2d_1x1 = "XC_conv2d_1x1"
     XC_conv2d_depthwise = "XC_conv2d_depthwise"
     XC_bsign_8 = "XC_bsign_8"
-    XC_bconv_bin_DIDO = "XC_bconv_bin_DIDO"
-    XC_bconv_int8_DIDO = "XC_bconv_int8_DIDO"
+    XC_bconv2d_bin_out = "XC_bconv2d_bin_out"
+    XC_bconv2d_int8_out = "XC_bconv2d_int8_out"
     XC_pad = "XC_pad"
 
 

d7fcbca86c91f9508649f077d15b72102b1aed9c
Laszlo Kindrat <laszlokindrat@xmos.com>
2020-09-24 08:49:55 -0400

    fixed reshape passes to do the right thing in mobilenetv1

--- tflite2xcore/tflite2xcore/transformation_passes/reshape_passes.py
+++ tflite2xcore/tflite2xcore/transformation_passes/reshape_passes.py
@@ -19,26 +19,24 @@ class RemoveFlattenReshapePass(OperatorMatchingPass):
         return self._op.inputs[0].producers[0]
 
     def match(self, op: Operator) -> bool:
-        with self.using(op):
-            try:
-                producer = self._producer
-            except IndexError:
-                # Input tensor for op has no producers..
-                return False
+        if super().match(op) and op.operator_code.code in self.MATCHING_OPCODES:
+            with self.using(op):
+                try:
+                    producer = self._producer
+                except IndexError:
+                    return False
 
-        # FULLY_CONNECTED always interprets the first dim as batch...
-        reshape_input_batch = producer.inputs[0].shape[0]
-        reshape_output_batch = op.inputs[0].shape[0]
+            reshape_input_batch = producer.inputs[0].shape[0]
+            reshape_output_batch = op.inputs[0].shape[0]
 
-        return (
-            super().match(op)
-            and op.operator_code.code in self.MATCHING_OPCODES
-            and producer.operator_code.code is BuiltinOpCodes.RESHAPE
-            and reshape_output_batch == reshape_input_batch
-        )
+            return (
+                producer.operator_code.code is BuiltinOpCodes.RESHAPE
+                and reshape_output_batch == reshape_input_batch
+            )
+
+        return False
 
     def mutate(self, op: Operator) -> None:
-
         with self.using(op):
             producer = self._producer
 
@@ -58,12 +56,15 @@ class CanonicalizeReshapePass(OperatorMatchingPass):
 
         try:
             if list(op.builtin_options["new_shape"]) != list(op.outputs[0].shape):
-                self.logger.warning(
+                raise ValueError(
                     "new_shape option to RESHAPE doesn't match output tensor shape"
                 )
         except (KeyError, TypeError):
-            # TODO: consider removing this since in tf2.2 the builtin options seems unused
-            self.logger.warning("Expected new_shape option to RESHAPE was not found")
+            # in tf2.2 the builtin options seems unused
+            self.logger.debug(
+                "Expected new_shape option to RESHAPE was not found "
+                "(ensure you are running tf2.2 or newer)"
+            )
 
         if -1 in op.inputs[0].shape + op.outputs[0].shape:
             self.logger.warning("Dynamically sized tensors are not supported")
@@ -73,7 +74,11 @@ class CanonicalizeReshapePass(OperatorMatchingPass):
             op.outputs[0].shape
         ), "RESHAPE input and output shapes are not consistent"
 
-        return len(op.inputs) == 2 and op.inputs[1].is_constant
+        # NOTE: we used to check if op.inputs[1] is constant
+        #       However since we neither we or the runtime currently supports
+        #       dynamic shapes, this is disabled for now to enable better
+        #       conversion of certain models (e.g. mobilenet v1) in tf2.3>
+        return len(op.inputs) == 2
 
     def mutate(self, op: Operator) -> None:
         # Remove connection between RESHAPE and input tensor[1], the new shape

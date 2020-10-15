d48672c287f44899091acc30f57c1a2bc95ece8d
Scott Roset <scottroset@Scotts-MacBook-Pro.local>
2020-10-15 16:38:01 -0400

    added float warning test

---
+++ tflite2xcore/tflite2xcore/tests/test_transformation_passes/test_warning_passes/test_FloatingPointWarningPass.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2020, XMOS Ltd, All rights reserved
+
+import pytest
+
+from tflite2xcore.transformation_passes import FloatingPointWarningPass
+from tflite2xcore.xcore_model import XCOREModel
+from tflite2xcore.xcore_schema import TensorType
+
+_TENSOR_SHAPE = (1, 1, 1, 1)
+
+#  ----------------------------------------------------------------------------
+#                                   FIXTURES
+#  ----------------------------------------------------------------------------
+
+
+@pytest.fixture()
+def trf_pass() -> FloatingPointWarningPass:
+    return FloatingPointWarningPass()
+
+
+@pytest.fixture()
+def tensor_type() -> TensorType:
+    return TensorType.FLOAT64
+
+
+@pytest.fixture()
+def non_matching_tensor_type() -> TensorType:
+    return TensorType.INT8
+
+
+@pytest.fixture()
+def model(tensor_type: TensorType) -> XCOREModel:
+    model = XCOREModel()
+    subgraph = model.create_subgraph()
+    subgraph.create_tensor("test_tensor", tensor_type, _TENSOR_SHAPE)
+    return model
+
+
+#  ----------------------------------------------------------------------------
+#                                   TESTS
+#  ----------------------------------------------------------------------------
+
+
+def test_matching_params(trf_pass: FloatingPointWarningPass, model: XCOREModel) -> None:
+    assert trf_pass.match(model.subgraphs[0].tensors[0])
+
+
+def test_non_matching_params(
+    trf_pass: FloatingPointWarningPass, non_matching_tensor_type: TensorType
+) -> None:
+    subgraph = XCOREModel().create_subgraph()
+    test_tensor = subgraph.create_tensor(
+        "test_tensor", non_matching_tensor_type, _TENSOR_SHAPE
+    )
+    assert not trf_pass.match(test_tensor)
+
+
+if __name__ == "__main__":
+    pytest.main()

5e11fdfd5be48fd40175fd89e7bf35ffe21930ae
Scott Roset <scottroset@Scotts-MacBook-Pro.local>
2020-11-27 15:54:10 -0500

    fixed 1 tensor for bias_scale_shift, test_mutate

--- tflite2xcore/tflite2xcore/tests/test_transformation_passes/test_add_passes/test_ReplaceAddPass.py
+++ tflite2xcore/tflite2xcore/tests/test_transformation_passes/test_add_passes/test_ReplaceAddPass.py
@@ -2,7 +2,6 @@
 
 import pytest
 from typing import Tuple
-from copy import deepcopy
 
 
 from tflite2xcore.transformation_passes import ReplaceAddPass
@@ -14,7 +13,7 @@ from tflite2xcore.xcore_schema import (
     XCOREOpCodes,
     Subgraph,
 )
-from . import test_replace_mutate as test_mutate
+from . import test_replace_mutate as _test_replace_mutate
 
 
 #  ----------------------------------------------------------------------------
@@ -26,27 +25,26 @@ def build_add(
     subgraph: Subgraph = None, *, input_shape: Tuple[int, ...], tensor_type: TensorType
 ) -> XCOREModel:
     subgraph = subgraph or XCOREModel().create_subgraph()
-    quantization = {"scale": [0.35], "zero_point": [0]}
     input_tensor_0 = subgraph.create_tensor(
         "input_0",
         tensor_type,
         input_shape,
         isinput=True,
-        quantization=deepcopy(quantization),
+        quantization={"scale": [0.35], "zero_point": [0]},
     )
     input_tensor_1 = subgraph.create_tensor(
         "input_1",
         tensor_type,
         input_shape,
         isinput=True,
-        quantization=deepcopy(quantization),
+        quantization={"scale": [0.15], "zero_point": [2]},
     )
     output_tensor = subgraph.create_tensor(
         "output",
         tensor_type,
         input_shape,
         isoutput=True,
-        quantization=deepcopy(quantization),
+        quantization={"scale": [0.25], "zero_point": [1]},
     )
     subgraph.create_operator(
         OperatorCode(BuiltinOpCodes.ADD),
@@ -111,6 +109,27 @@ def test_non_matching_tensor_shape(trf_pass: ReplaceAddPass, model: XCOREModel) 
     new_shape = (current_shape[0] + 1, *current_shape[1:])
     model.subgraphs[0].get_tensor("input_1").shape = new_shape
     assert not trf_pass.match(model.subgraphs[0].operators[0])
+
+
+def test_mutate(
+    trf_pass: ReplaceAddPass, model: XCOREModel, new_opcode: XCOREOpCodes
+) -> None:
+    # run mutating pass
+    trf_pass.run(model)
+    model.sanity_check()
+
+    _test_replace_mutate(trf_pass, model, new_opcode)
+
+    # check operators
+    subgraph = model.subgraphs[0]
+    operators = subgraph.operators
+    assert len(operators) == 1
+    op = operators[0]
+
+    # check tensors
+    assert len(op.inputs) == 3
+    assert len(op.outputs) == 1
+    assert op.inputs[2].shape == (6,)
 
 
 if __name__ == "__main__":
--- tflite2xcore/tflite2xcore/transformation_passes/add_passes.py
+++ tflite2xcore/tflite2xcore/transformation_passes/add_passes.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2020, XMOS Ltd, All rights reserved
 
 from typing import Iterable
-
+from copy import copy
+from pprint import pprint
 import numpy as np
 
 from tflite2xcore.xcore_model import Operator, Subgraph
@@ -33,93 +34,77 @@ class ReplaceAddPass(ReplaceQuantizedOperatorPass):
             and op.inputs[0].shape == op.inputs[1].shape == op.outputs[0].shape
         )
 
-    def mutate(self, op):
+    def mutate(self, op: Operator):
+        old_op = copy(op)
         new_op = super().mutate(op)
-        subgraph = new_op.subgraph
 
-        with self.using(new_op):
-            # replace reduction_indices tensor with bias_scale_shift
-            new_op.inputs[0].consumers.remove(new_op)
-            new_op.inputs[0] = subgraph.create_tensor(
-                f"{new_op.name}/bias_scale_shift",
-                TensorType.INT8,
-                shape=[7],
-                consumers=[new_op],
-            )
+        # passed variables
+        s_0 = 0
+        s_1 = 0
 
-            # replace reduction_indices tensor with bias_scale_shift
-            new_op.inputs[1].consumers.remove(new_op)
-            new_op.inputs[1] = subgraph.create_tensor(
-                f"{new_op.name}/bias_scale_shift",
-                TensorType.INT8,
-                shape=[7],
-                consumers=[new_op],
-            )
+        # calculate scale_mismatch
+        scale0_scaleOut = (
+            old_op.inputs[0].quantization["scale"][0]
+            / old_op.outputs[0].quantization["scale"][0]
+        )
+        scale1_scaleOut = (
+            old_op.inputs[1].quantization["scale"][0]
+            / old_op.outputs[0].quantization["scale"][0]
+        )
 
-            # replace reduction_indices tensor with bias_scale_shift
-            new_op.outputs[0].producers.remove(new_op)
-            new_op.outputs[0] = subgraph.create_tensor(
-                f"{new_op.name}/bias_scale_shift",
-                TensorType.INT8,
-                shape=[7],
-                producers=[new_op],
-            )
+        n = max(
+            scale0_scaleOut,
+            scale1_scaleOut,
+        )
 
-            # passed variables
-            s_0 = 0
-            s_1 = 0
+        msb = 0
+        n = int(n / 2)
 
-            # calculate scale_mismatch
-            scale0_scaleOut = (
-                self._input.quantization["scale"][0]
-                / self._output.quantization["scale"][0]
-            )
-            scale1_scaleOut = (
-                self._input.quantization["scale"][1]
-                / self._output.quantization["scale"][0]
-            )
-
-            n = max(
-                scale0_scaleOut,
-                scale1_scaleOut,
-            )
-
-            msb = 0
+        while n > 0:
             n = int(n / 2)
+            msb += 1
 
-            while n > 0:
-                n = int(n / 2)
-                msb += 1
+        scale_mismatch = 16 - (msb + 1)
 
-            scale_mismatch = 16 - (msb + 1)
+        m_0 = scale0_scaleOut * 2 ** scale_mismatch
+        m_1 = scale1_scaleOut * 2 ** scale_mismatch
 
-            m_0 = scale0_scaleOut << scale_mismatch
-            m_1 = scale1_scaleOut << scale_mismatch
+        b = (
+            (old_op.outputs[0].quantization["zero_point"][0] << scale_mismatch)
+            - m_0 * old_op.inputs[0].quantization["zero_point"][0]
+            - m_1 * old_op.inputs[1].quantization["zero_point"][0]
+        )
 
-            b = (
-                (self._output.quantization["zero_point"][0] << scale_mismatch)
-                - m_0 * self._input.quantization["zero_point"][0]
-                - m_1 * self._input.quantization["zero_point"][1]
-            )
+        s_out = scale_mismatch
 
-            s_out = scale_mismatch
+        subgraph = new_op.subgraph
+        pprint(vars(new_op))
+        bias_scale_shift_tensor = subgraph.create_tensor(
+            f"{new_op.name}/bias_scale_shift",
+            TensorType.INT32,
+            consumers=[new_op],
+            shape=[6],
+        )
+        new_op.inputs.append(bias_scale_shift_tensor)
 
-            new_op.inputs[0].buffer.data = b"".join(
-                # p.tostring() for p in self._bias_scale_shift
-                p.tostring()
-                for p in [m_0.astype(np.int16), s_0]
-            )
+        m_0 = np.array(m_0)
+        m_1 = np.array(m_1)
+        s_0 = np.array(s_0)
+        s_1 = np.array(s_1)
+        s_out = np.array(s_out)
+        b = np.array(b)
 
-            new_op.inputs[1].buffer.data = b"".join(
-                # p.tostring() for p in self._bias_scale_shift
-                p.tostring()
-                for p in [m_1.astype(np.int16), s_1]
-            )
-
-            new_op.outputs[0].buffer.data = b"".join(
-                # p.tostring() for p in self._bias_scale
-                p.tostring()
-                for p in [b.astype(np.int32), s_out]
-            )
+        new_op.inputs[2].buffer.data = b"".join(
+            # p.tostring() for p in self._bias_scale_shift
+            p.tostring()
+            for p in [
+                b.astype(np.int32),
+                m_0.astype(np.int32),
+                m_1.astype(np.int32),
+                s_0.astype(np.int32),
+                s_1.astype(np.int32),
+                s_out.astype(np.int32),
+            ]
+        )
 
         return new_op

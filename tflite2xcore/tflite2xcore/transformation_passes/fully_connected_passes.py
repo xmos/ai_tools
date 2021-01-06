044e0952bfab4bdf41732578a3f748364f90d60b
Laszlo Kindrat <laszlokindrat@xmos.com>
2021-01-06 10:26:31 -0500

    cleaned up vpu constant names in the xformer

--- tflite2xcore/tflite2xcore/transformation_passes/fully_connected_passes.py
+++ tflite2xcore/tflite2xcore/transformation_passes/fully_connected_passes.py
@@ -7,7 +7,7 @@ from tflite2xcore.xcore_schema import (
     OperatorCode,
     XCOREOpCodes,
 )
-from tflite2xcore.utils import WORD_SIZE
+from tflite2xcore.utils import WORD_SIZE_BYTES
 
 from .transformation_passes import (
     ReplaceXCWeightBiasOperatorPass,
@@ -36,7 +36,9 @@ class LegalizeXCFullyConnectedPass(LegalizeXCWeightBiasPass):
     def mutate_weights(self, op):
         with self.using(op):
             # zero_padding weight tensor
-            col_pad = WORD_SIZE - 1 - (self._weights.shape[1] - 1) % WORD_SIZE
+            col_pad = (
+                WORD_SIZE_BYTES - 1 - (self._weights.shape[1] - 1) % WORD_SIZE_BYTES
+            )
             arr = np.pad(self._weights.as_array(), pad_width=[(0, 0), (0, col_pad)])
 
             self._replace_weights(arr)

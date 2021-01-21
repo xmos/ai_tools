237e5403807791348d8090e8ec7bb09efbdb9b32
Andrew Cavanaugh <andrewc@xmos.com>
2021-01-21 16:05:04 -0500

    Experiment/mlir (#379)
    * Initial stub of mlir experiment

---
+++ experimental/mlir/hello-world/lib-test.cc
@@ -0,0 +1,10 @@
+#include "lib-test.h"
+#include <cstdio>
+#include <math.h>
+
+#include "tensorflow/compiler/mlir/lite/flatbuffer_import.h"
+
+void print_some_stuff()
+{
+    printf("Some stuff %0.8f \n", M_PI);
+}

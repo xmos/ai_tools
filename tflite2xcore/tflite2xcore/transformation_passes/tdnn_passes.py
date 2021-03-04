395626e4cdc4caed38f02e3875c8cae06989c6d2
scott-xmos <scottroset@xmos.com>
2021-03-04 16:24:19 -0500

    prototype TDNN xformer pass w/ unit test

--- tflite2xcore/tflite2xcore/transformation_passes/tdnn_passes.py
+++ tflite2xcore/tflite2xcore/transformation_passes/tdnn_passes.py
@@ -1,26 +1,52 @@
 # Copyright 2021 XMOS LIMITED. This Software is subject to the terms of the
 # XMOS Public License: Version 1
-from pprint import pprint
-from typing import Iterable
 
-from tflite2xcore.xcore_model import Operator, Subgraph
-from tflite2xcore.xcore_schema import BuiltinOpCodes, OperatorCode, XCOREOpCodes
+from tflite2xcore.xcore_model import Operator
+from tflite2xcore.xcore_schema import (
+    OperatorCode,
+    XCOREOpCodes,
+    TensorType,
+)
 
 from .conv2d_passes import ReplaceDeepConv2dPass
 
 
 class TdnnDeepConv2DPass(ReplaceDeepConv2dPass):
     def match(self, op: Operator) -> bool:
-        return super().match(op)
+        return super().match(op) and "tdnn" not in op.custom_options
 
     def mutate(self, op: Operator):
-        # print(vars(op.inputs[0]))
-        # print((op.inputs[1]._shape[1:3]))
-        # pprint(vars(op.inputs[1]))
+        xc_conv_op = super().mutate(op)
 
-        new_op = super().mutate(op)
-        new_op.add_custom_options(tdnn=True)
+        xc_conv_op.add_custom_options(tdnn=True)
 
-        pprint(vars(new_op))
+        subgraph = xc_conv_op.subgraph
 
-        return new_op
+        ring_buffer_shape = list(xc_conv_op.inputs[0].shape)
+        # kernel_size[0]
+        ring_buffer_shape[1] = xc_conv_op.inputs[1].shape[0]
+        ring_buffer_tensor = subgraph.create_tensor(
+            f"{xc_conv_op.name}/ring_buffer",
+            TensorType.INT8,
+            consumers=[xc_conv_op],
+            shape=ring_buffer_shape,
+        )
+
+        old_data_shape = ring_buffer_shape
+        old_data_shape[1] = old_data_shape[1] - 1
+        old_data_tensor = subgraph.create_tensor(
+            f"{xc_conv_op.name}/old_data", TensorType.INT8, shape=old_data_shape
+        )
+
+        xc_conv_op.inputs[0].consumers.pop(0)
+
+        subgraph.create_operator(
+            OperatorCode(XCOREOpCodes.XC_ring_buffer),
+            inputs=[xc_conv_op.inputs[0], old_data_tensor],
+            outputs=[ring_buffer_tensor, old_data_tensor],
+        )
+        ring_buffer_op = subgraph.operators[-1]
+
+        xc_conv_op.inputs[0] = ring_buffer_tensor
+
+        return xc_conv_op

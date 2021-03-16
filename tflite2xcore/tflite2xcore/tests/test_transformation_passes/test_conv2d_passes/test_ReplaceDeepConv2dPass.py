425eb8f9fd7a60d2e9c7a40123283bb28b1c541d
Laszlo Kindrat <laszlokindrat@xmos.com>
2021-03-16 18:19:51 -0400

    fixed failing conv2d pass unit tests

--- tflite2xcore/tflite2xcore/tests/test_transformation_passes/test_conv2d_passes/test_ReplaceDeepConv2dPass.py
+++ tflite2xcore/tflite2xcore/tests/test_transformation_passes/test_conv2d_passes/test_ReplaceDeepConv2dPass.py
@@ -30,17 +30,17 @@ from .conftest import (
 #  ----------------------------------------------------------------------------
 
 
-@pytest.fixture()
+@pytest.fixture()  # type: ignore
 def build_model() -> ModelBuilder:
     return build_conv2d
 
 
-@pytest.fixture()
+@pytest.fixture()  # type: ignore
 def trf_pass() -> ReplaceDeepConv2dPass:
     return ReplaceDeepConv2dPass()
 
 
-@pytest.fixture()
+@pytest.fixture()  # type: ignore
 def model(
     weight_shape: Tuple[int, int, int, int],
     input_size: Tuple[int, int],
@@ -55,7 +55,7 @@ def model(
     )
 
 
-@pytest.fixture()
+@pytest.fixture()  # type: ignore
 def custom_opcode() -> XCOREOpCodes:
     return XCOREOpCodes.XC_conv2d_deep
 
@@ -71,7 +71,6 @@ def test_mutate(
     subgraph = model.subgraphs[0]
     old_op = subgraph.operators[0]
     strides = tuple(old_op.builtin_options[f"stride_{ax}"] for ax in ("h", "w"))
-    input_zero_point = old_op.inputs[0].quantization["zero_point"][0]
 
     _test_mutate(trf_pass, model, custom_opcode)
 
@@ -80,8 +79,7 @@ def test_mutate(
     assert custom_options["stride"] == strides
 
     assert "pad" in custom_options
-    assert len(custom_options["pad"]) == 3
-    assert custom_options["pad"][-1] == input_zero_point
+    assert len(custom_options["pad"]) == 2
 
 
 if __name__ == "__main__":

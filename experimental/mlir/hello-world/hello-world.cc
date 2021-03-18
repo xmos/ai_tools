2e8e04bc65af75c6858d15bf85fd275acd386358
Andrew Cavanaugh <andrewc@xmos.com>
2021-03-18 11:37:26 -0400

    merge conflict resolution

--- experimental/mlir/hello-world/hello-world.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 XMOS LIMITED.
-// This Software is subject to the terms of the XMOS Public Licence: Version 1.
-#include "lib-test.h"
-
-int main()
-{
-    print_some_stuff();
-    return 0;
-}

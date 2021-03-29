af352b968b3dcd16ebb60f00866e1e05edc4516d
Deepak Panickal <deepakpanickal@xmos.com>
2021-03-29 22:33:39 +0100

    Initial refactor

---
+++ experimental/mlir/transforms/passes.h
@@ -0,0 +1,37 @@
+#ifndef LARQ_COMPUTE_ENGINE_MLIR_PASSES_H_
+#define LARQ_COMPUTE_ENGINE_MLIR_PASSES_H_
+
+#include "mlir/Pass/Pass.h"
+
+// enum LCETarget { ARM = 0, XCORE = 1 };
+
+namespace mlir {
+namespace xcore {
+
+// Creates an instance of the TensorFlow dialect OpRemoval pass.
+std::unique_ptr<OperationPass<FuncOp>> CreatePrintNestingPass();
+
+/*
+// Creates an instance of the TensorFlow dialect OpRemoval pass.
+std::unique_ptr<OperationPass<FuncOp>> CreateOpRemovalPass();
+
+// Creates an instance of the TensorFlow dialect PrepareLCE pass.
+std::unique_ptr<OperationPass<FuncOp>> CreatePrepareLCEPass(LCETarget target);
+
+// Creates an instance of the TensorFlow dialect OptimizeLCE pass.
+std::unique_ptr<OperationPass<FuncOp>> CreateOptimizeLCEPass(
+    LCETarget target, bool experimental_enable_bitpacked_activations);
+
+// Creates an instance of the TensorFlow dialect BitpackWeightsLCE pass.
+std::unique_ptr<OperationPass<FuncOp>> CreateBitpackWeightsLCEPass();
+
+// Creates an instance of the TensorFlow Lite dialect QuantizeTFL pass.
+std::unique_ptr<OperationPass<FuncOp>> CreateLCEQuantizePass();
+
+// Creates an instance of LegalizeLCE pass.
+std::unique_ptr<OperationPass<FuncOp>> CreateLegalizeLCEPass();
+*/
+} // namespace xcore
+} // namespace mlir
+
+#endif // LARQ_COMPUTE_ENGINE_MLIR_PASSES_H_

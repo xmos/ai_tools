ae09a75769af1dea0ef5ccce5c4c5ea469087987
scott-xmos <scottroset@xmos.com>
2021-04-12 13:33:10 -0400

    tdnn option in xformer

--- tflite2xcore/xformer.py
+++ tflite2xcore/xformer.py
@@ -70,6 +70,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "--tdnn", action="store_true", default=False, help="Experimental flag - Convert network to tdnn",
     )
+    parser.add_argument(
+        "--tdnn", action="store_true", default=False, help="Convert network to tdnn",
+    )
 
     args = parser.parse_args()
 

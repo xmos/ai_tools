48a62114885b66631c913a1dced74ea45b2a9cbc
scott-xmos <scottroset@xmos.com>
2021-04-12 13:33:10 -0400

    tdnn option in xformer

--- tflite2xcore/tflite2xcore/converter.py
+++ tflite2xcore/tflite2xcore/converter.py
@@ -125,12 +125,15 @@ class ActivationLoweringManager(PassManager):
 
 class PoolingLoweringManager(PassManager):
     def __init__(
-        self, model: Optional[XCOREModel] = None,  **kwargs: Any
+        self, model: Optional[XCOREModel] = None, tdnn: bool = False, **kwargs: Any
     ) -> None:
         super().__init__(model, **kwargs)
 
-        self.register_pass(passes.ReplaceMaxPool2D2x2Pass())
-        self.register_pass(passes.ReplaceMaxPool2DPass())
+        if tdnn:
+            self.register_pass(passes.TdnnMaxPool2DPass())
+        else:
+            self.register_pass(passes.ReplaceMaxPool2D2x2Pass())
+            self.register_pass(passes.ReplaceMaxPool2DPass())
         self.register_pass(passes.ReplaceAveragePool2D2x2Pass())
         self.register_pass(passes.ReplaceAveragePool2DPass())
         self.register_pass(passes.ReplaceGlobalAveragePool2DPass())

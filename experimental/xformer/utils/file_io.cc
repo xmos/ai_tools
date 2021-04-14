6798e877a81d602d083f67f20aa13c401b0590d5
Deepak Panickal <deepakpanickal@xmos.com>
2021-04-14 11:07:28 +0100

    Add newlines, mlir_example.tflite, and copyright

--- experimental/xformer/utils/file_io.cc
+++ experimental/xformer/utils/file_io.cc
@@ -1,3 +1,6 @@
+// Copyright 2021 XMOS LIMITED. This Software is subject to the terms of the
+// XMOS Public License: Version 1
+
 #include "utils/file_io.h"
 
 #include "mlir/Support/FileUtilities.h"

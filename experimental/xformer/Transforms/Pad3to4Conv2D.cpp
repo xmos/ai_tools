11ee17d56a743529111cd795ef2d1065eb725df5
Deepak Panickal <deepakpanickal@xmos.com>
2021-09-17 11:17:06 +0100

    Cleanup pad 3-4

--- experimental/xformer/Transforms/Pad3to4Conv2D.cpp
+++ experimental/xformer/Transforms/Pad3to4Conv2D.cpp
@@ -33,6 +33,14 @@ struct Pad3to4Conv2DPattern : public OpRewritePattern<TFL::Conv2DOp> {
   LogicalResult matchAndRewrite(TFL::Conv2DOp conv2DOp,
                                 PatternRewriter &rewriter) const override {
     // Check for invalid types and return
+    // We don't bother padding if output depth is not a multiple of four as we
+    // cannot optimize it with an XC Conv2D
+    auto outputDepth =
+        conv2DOp.output().getType().cast<ShapedType>().getDimSize(3);
+    if (outputDepth % 4 != 0) {
+      return failure();
+    }
+
     // Input depth must be three
     auto inputDepth =
         conv2DOp.input().getType().cast<ShapedType>().getDimSize(3);
@@ -40,83 +48,66 @@ struct Pad3to4Conv2DPattern : public OpRewritePattern<TFL::Conv2DOp> {
       return failure();
     }
 
-    // pad op test
-    RankedTensorType paddings_ty =
+    // Pad the Conv2D input
+    RankedTensorType paddingsType =
         RankedTensorType::get({4, 2}, rewriter.getI32Type());
-    std::vector<int32_t> paddings_values = {0, 0, 0, 0, 0, 0, 0, 1};
+
+    // Pad the input depth by one, i.e, from 3 to 4
+    std::vector<int32_t> paddingsValues = {0, 0, 0, 0, 0, 0, 0, 1};
     Value paddings = rewriter.create<TFL::ConstOp>(
         conv2DOp.getLoc(),
-        DenseIntElementsAttr::get(paddings_ty, paddings_values));
-
-    auto input_shape =
+        DenseIntElementsAttr::get(paddingsType, paddingsValues));
+    auto inputShape =
         conv2DOp.input().getType().cast<RankedTensorType>().getShape();
-    std::vector<int64_t> pad_shape = {input_shape[0], input_shape[1],
-                                      input_shape[2], input_shape[3] + 1};
-    SmallVector<int64_t, 4> expand_shape(pad_shape.begin(), pad_shape.end());
-    auto expand_result_type = RankedTensorType::get(
-        expand_shape,
+
+    // The pad output depth would increse from 3 to 4
+    auto paddedInputResultType = RankedTensorType::get(
+        {inputShape[0], inputShape[1], inputShape[2], inputShape[3] + 1},
         conv2DOp.input().getType().cast<ShapedType>().getElementType());
 
-    Value padOutput = rewriter.create<TFL::PadOp>(
-        conv2DOp.getLoc(), expand_result_type, conv2DOp.input(), paddings);
+    // Set the PadOp output as the Conv2D input
+    Value padOpOutput = rewriter.create<TFL::PadOp>(
+        conv2DOp.getLoc(), paddedInputResultType, conv2DOp.input(), paddings);
+    conv2DOp.setOperand(0, padOpOutput);
 
-    conv2DOp.setOperand(0, padOutput);
-
-    // Pad the filter accordingly
-    // We need to do this at compile time instead of using a PadOp as we use the
-    // padded filter values for the boggling calculations Get filter values
+    // Pad the Conv2D filter
+    // We need to do this at compile time instead of using a PadOp
+    // as we use the padded filter values for the boggling calculations
+    // for creating the XC Conv2D ops
     auto filterQConstOp =
         dyn_cast<TFL::QConstOp>(conv2DOp.filter().getDefiningOp());
     auto filter = filterQConstOp.value().cast<DenseElementsAttr>();
     auto filterVector = std::vector<int8_t>{filter.getValues<int8_t>().begin(),
                                             filter.getValues<int8_t>().end()};
-
-    auto filter_shape =
+    auto filterShape =
         conv2DOp.filter().getType().cast<RankedTensorType>().getShape();
-    std::vector<int64_t> pad_filter_shape = {
-        filter_shape[0], filter_shape[1], filter_shape[2], filter_shape[3] + 1};
-
     llvm::SmallVector<int8_t, 0> paddedFilterVector;
-    paddedFilterVector.reserve(pad_filter_shape[0] * pad_filter_shape[1] *
-                               pad_filter_shape[2] * pad_filter_shape[3]);
+    paddedFilterVector.reserve(filterShape[0] * filterShape[1] *
+                               filterShape[2] * (filterShape[3] + 1));
 
-    for (int i = 0; i < filterVector.size(); i += filter_shape[3]) {
+    // Pad the filter depth by one, i.e, from 3 to 4
+    for (int i = 0; i < filterVector.size(); i += filterShape[3]) {
       paddedFilterVector.insert(paddedFilterVector.end(),
                                 filterVector.begin() + i,
-                                filterVector.begin() + i + filter_shape[3]);
+                                filterVector.begin() + i + filterShape[3]);
       paddedFilterVector.insert(paddedFilterVector.end(), 0);
     }
 
-    SmallVector<int64_t, 4> expand_filter_shape(pad_filter_shape.begin(),
-                                                pad_filter_shape.end());
-    auto expand_filter_result_type = RankedTensorType::get(
-        expand_filter_shape,
+    auto paddedFilterResultType = RankedTensorType::get(
+        {filterShape[0], filterShape[1], filterShape[2], filterShape[3] + 1},
         conv2DOp.filter().getType().cast<ShapedType>().getElementType());
+    RankedTensorType paddedFilterValueType = RankedTensorType::get(
+        {filterShape[0], filterShape[1], filterShape[2], filterShape[3] + 1},
+        rewriter.getIntegerType(8));
 
-    RankedTensorType newFilterType =
-        RankedTensorType::get(expand_filter_shape, rewriter.getIntegerType(8));
+    // Create a new QConstOp with the padded data
+    Value paddedQConstOpOutput = rewriter.create<TFL::QConstOp>(
+        conv2DOp.getLoc(), mlir::TypeAttr::get(paddedFilterResultType),
+        DenseElementsAttr::get<int8_t>(paddedFilterValueType,
+                                       paddedFilterVector));
 
-    Value padFilterOutput = rewriter.create<TFL::QConstOp>(
-        conv2DOp.getLoc(), mlir::TypeAttr::get(expand_filter_result_type),
-        DenseElementsAttr::get<int8_t>(newFilterType, paddedFilterVector));
-
-    // Value padFilterOutput = rewriter.create<TFL::PadOp>(
-    //     conv2DOp.getLoc(), expand_filter_result_type, conv2DOp.filter(),
-    //     paddings);
-
-    conv2DOp.setOperand(1, padFilterOutput);
-
-    // // Create the Conv2DV2 Op with the params and kernel type
-    // auto newConv2DV2Op = rewriter.create<Conv2DV2Op>(
-    //     conv2DOp.getLoc(), conv2DOp.getType(), output,
-    //     rewriter.getI32IntegerAttr(threadCount),
-    //     rewriter.getI32ArrayAttr(scratchByteParams),
-    //     getStringArrayAttr(abstractKernelParams),
-    //     getStringArrayAttr(memcpyFnParams),
-    //     getStringArrayAttr(aggregateFnParams),
-    //     getStringArrayAttr(outputTransformFnParams),
-    //     getStringArrayAttr(kernelTypeEnumParams));
-    // rewriter.replaceOp(conv2DOp, newConv2DV2Op.output());
+    // Set the QConstOp output as the Conv2D filter
+    conv2DOp.setOperand(1, paddedQConstOpOutput);
 
     return success();
   }

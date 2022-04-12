12a01a8bcd5b7f9de621da2ebd4c6f7ac2797252
scott-xmos <scottroset@xmos.com>
2022-04-12 10:47:28 -0400

    read 2 different configs from yaml, write 2 files

--- test/integration_test/generate_models/generate_strided_slice.py
+++ test/integration_test/generate_models/generate_strided_slice.py
@@ -9,32 +9,33 @@ dir = os.path.dirname(__file__)
 with open(os.path.join(dir,'test_strided_slice.yml'), 'r') as file:
 	configs = yaml.safe_load(file)
 
-height = configs['default'][0]['height']
-width = configs['default'][0]['width']
-channels = configs['default'][0]['channels']
+for i in range(len(configs['default'])):
+	height = configs['default'][i]['height']
+	width = configs['default'][i]['width']
+	channels = configs['default'][i]['channels']
 
-inputs = tf.keras.Input(shape=(height,width,channels),)
-x_0 = tf.strided_slice(inputs,begin=[0, 0, 0], end= [height, width//2, channels],strides= [1, 1, 1])
-x_1 = tf.strided_slice(inputs,begin=[0, ceil(width/2), 0], end= [height, width, channels],strides= [1, 1, 1])
-x = tf.keras.layers.Concatenate(axis=1)([x_0,x_1])
-outputs = tf.keras.layers.Flatten()(x)
-model = tf.keras.Model(inputs=inputs,outputs=[outputs])
+	inputs = tf.keras.Input(shape=(height,width,channels),)
+	x_0 = tf.strided_slice(inputs,begin=[0, 0, 0], end= [height, width//2, channels],strides= [1, 1, 1])
+	x_1 = tf.strided_slice(inputs,begin=[0, ceil(width/2), 0], end= [height, width, channels],strides= [1, 1, 1])
+	x = tf.keras.layers.Concatenate(axis=1)([x_0,x_1])
+	outputs = tf.keras.layers.Flatten()(x)
+	model = tf.keras.Model(inputs=inputs,outputs=[outputs])
 
-saved_model_dir = os.path.join(dir,"saved_model")
-model.save(saved_model_dir)
+	saved_model_dir = os.path.join(dir,"saved_model")
+	model.save(saved_model_dir)
 
-def representative_dataset():
-    for _ in range(100):
-      data = np.random.rand(1,height,width,channels)
-      yield [data.astype(np.float32)]
+	def representative_dataset():
+		for _ in range(100):
+			data = np.random.rand(1,height,width,channels)
+			yield [data.astype(np.float32)]
 
-converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.representative_dataset = representative_dataset
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-converter.inference_input_type = tf.int8  # or tf.uint8
-converter.inference_output_type = tf.int8  # or tf.uint8
-tflite_quant_model = converter.convert()
+	converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+	converter.optimizations = [tf.lite.Optimize.DEFAULT]
+	converter.representative_dataset = representative_dataset
+	converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+	converter.inference_input_type = tf.int8  # or tf.uint8
+	converter.inference_output_type = tf.int8  # or tf.uint8
+	tflite_quant_model = converter.convert()
 
-with open(os.path.join(dir,"strided_slice.tflite"),mode='wb') as file:
-  file.write(tflite_quant_model)
+	with open(os.path.join(dir,"strided_slice_"+str(i)+".tflite"),mode='wb') as file:
+		file.write(tflite_quant_model)

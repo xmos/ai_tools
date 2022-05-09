0fbaf04da98ce4c7ce5dca9f354f10f3da978bad
Deepak Panickal <deepakpanickal@xmos.com>
2022-05-09 14:12:34 +0100

    Update github actions linux

--- .github/scripts/build_linux_wheel.sh
+++ .github/scripts/build_linux_wheel.sh
@@ -4,7 +4,7 @@ set -e -x
 pip install cmake
 cd third_party/lib_tflite_micro
 # Use gcc7 toolchain from the docker file to build tflm_interpreter
-CC=/dt7/usr/bin/gcc CXX=/dt7/usr/bin/g++ make build
+#CC=/dt7/usr/bin/gcc CXX=/dt7/usr/bin/g++ make build
 
 # Build xcore-opt with an older gcc7 toolchain
 # Have to explicitly link with -lrt to prevent the following linking errors
@@ -16,7 +16,7 @@ CC=/dt7/usr/bin/gcc CXX=/dt7/usr/bin/g++ make build
 #/usr/bin/ld: bazel-out/k8-fastbuild/bin/external/com_google_absl/absl/base/libbase.a(sysinfo.pic.o): in function `absl::lts_20210324::base_internal::ReadMonotonicClockNanos()':
 #sysinfo.cc:(.text+0x156): undefined reference to `clock_gettime'
 cd ../../experimental/xformer
-bazel build //:xcore-opt --linkopt=-lrt --crosstool_top=@org_tensorflow//third_party/toolchains/preconfig/ubuntu16.04/gcc7_manylinux2010-nvcc-cuda11.2:toolchain
+#bazel build //:xcore-opt --linkopt=-lrt --crosstool_top=@org_tensorflow//third_party/toolchains/preconfig/ubuntu16.04/gcc7_manylinux2010-nvcc-cuda11.2:toolchain
 
 # Build python wheel
 cd python

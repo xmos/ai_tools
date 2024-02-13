04d2cb5148b3e50b3bfa40a8bff83b38d0391adf
Michael Poluektov <michael.poluektov@gmail.com>
2024-02-13 10:09:57 +0000

    remove unused tablegen pass

--- xformer/Utils/Util.h
+++ xformer/Utils/Util.h
@@ -4,9 +4,7 @@
 #ifndef XFORMER_UTILS_UTIL_H
 #define XFORMER_UTILS_UTIL_H
 
-#include "mlir/IR/BuiltinAttributes.h" // from @llvm-project
-#include "mlir/IR/BuiltinTypes.h"
-#include "llvm/ADT/ArrayRef.h"
+#include "mlir/IR/BuiltinTypeInterfaces.h"
 
 namespace mlir {
 namespace xcore {
@@ -15,30 +13,6 @@ namespace utils {
 int getShapedTypeSize(ShapedType t);
 
 LogicalResult hasSameShape(ShapedType type1, ShapedType type2);
-
-inline bool IsAllOnesConstant(Attribute value) {
-  auto values = value.cast<DenseElementsAttr>().getValues<int32_t>();
-  return !std::any_of(values.begin(), values.end(),
-                      [](int32_t element_value) { return element_value != 1; });
-}
-
-// Utility function to get the offset between two dense attribute values.
-inline TypedAttr GetOffSet(Attribute begin, Attribute end) {
-  auto begin_values = begin.cast<DenseElementsAttr>().getValues<int32_t>();
-  auto end_values = end.cast<DenseElementsAttr>().getValues<int32_t>();
-
-  SmallVector<int32_t> offsets;
-  if (begin_values.size() == end_values.size()) {
-    for (size_t i = 0; i < begin_values.size(); ++i) {
-      offsets.push_back(end_values[i] - begin_values[i]);
-    }
-  }
-
-  return mlir::DenseElementsAttr::get(
-      RankedTensorType::get({static_cast<int>(offsets.size())},
-                            mlir::IntegerType::get(begin.getContext(), 32)),
-      ArrayRef(offsets));
-}
 
 } // namespace utils
 } // namespace xcore

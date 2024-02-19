d2871eeb16342b8dd235dacee6de8224946cf0b5
Michael Poluektov <michael.poluektov@gmail.com>
2024-02-19 13:55:07 +0000

    fix device tests (hopefully?)

--- python/xmos_ai_tools/io_server/__init__.py
+++ python/xmos_ai_tools/io_server/__init__.py
@@ -70,6 +70,11 @@ class IOServer:
         assert type(data_read) is bytearray
         return self.bytes_to_arr(data_read, tensor_num)
 
+    def close(self):
+        if self._dev:
+            usb.util.dispose_resources(self._dev)
+            self._dev = None
+
     @handle_usb_error
     def _download_data(self, cmd, data_bytes, tensor_num=0, model_num=0):
         # TODO rm this extra CMD packet

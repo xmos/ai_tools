5524f83994631a34f13f3941091438311465fe98
Michael Poluektov <michael.poluektov@gmail.com>
2024-03-21 10:24:30 +0000

    update tests

--- integration_tests/models/16x8/test_concatenate/generate.py
+++ integration_tests/models/16x8/test_concatenate/generate.py
@@ -6,7 +6,7 @@ i = 0
 
 def generate_concatenate_model(input_shapes, axis):
     input_data = [tf.keras.Input(shape=input_shape, dtype=np.float32, batch_size=1) for input_shape in input_shapes]
-    concatenated_output = tf.keras.layers.Concatenate(axis=axis)(input_data)
+    concatenated_output = tf.concat(input_data, axis=axis)
     model = tf.keras.Model(inputs=input_data, outputs=concatenated_output)
     converter = tfl.TFLiteConverter.from_keras_model(model)
     def representative_dataset_gen():
@@ -28,6 +28,7 @@ def generate_concatenate_model(input_shapes, axis):
 
 generate_concatenate_model([(64), (64)], 0)
 generate_concatenate_model([(2, 3), (2, 3)], 1)
-generate_concatenate_model([(2, 3, 5), (2, 3, 5)], 0)
+generate_concatenate_model([(2, 3, 5), (2, 3, 5)], -1)
 generate_concatenate_model([(2, 6, 5, 2), (2, 6, 5, 2)], 1)
+generate_concatenate_model([(2, 6, 5, 2), (2, 6, 5, 2)], 3)
 

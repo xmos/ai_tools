4a8bf20f2fd2217b5696169d23bf5dbb320ed659
panickal-xmos <deepakpanickal@xmos.com>
2024-08-02 14:59:02 +0100

    Fix typo

--- xformer/Transforms/ReplaceConcat.cpp
+++ xformer/Transforms/ReplaceConcat.cpp
@@ -62,8 +62,8 @@ struct SplitConcatPattern : public OpRewritePattern<TFL::ConcatenationOp> {
     auto outputType = concatOp.getOutput().getType().cast<RankedTensorType>();
     Type elementType = outputType.getElementType();
     ArrayRef<int64_t> outputShape = outputType.getShape();
-    int axis = concatOp.getAxis();
-    axis = -1 ? outputType.getRank() - 1 : axis;
+    const int axis = concatOp.getAxis() == -1 ? outputType.getRank() - 1
+                                              : concatOp.getAxis();
 
     int axisShape = 0;
     for (int i = 0; i < CONCAT_OP_MAX_INPUTS; i++) {

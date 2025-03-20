a0704835517c05dd3748976c5b23071b7be5a234
panickal-xmos <deepakpanickal@xmos.com>
2025-03-20 10:26:41 +0000

    Allocate external memory

--- python/xmos_ai_tools/xinterpreters/host_interpreter.py
+++ python/xmos_ai_tools/xinterpreters/host_interpreter.py
@@ -127,8 +127,9 @@ class TFLMHostInterpreter:
         @param model_index  The model to target, for interpreters that support multiple models
         running concurrently. Defaults to 0 for use with a single model.
         """
-        max_model_size = 50000000
-        self.obj = lib.new_interpreter(max_model_size, max_model_size)
+        max_arena_size = 50000000
+        external_memory_size = 50000000
+        self.obj = lib.new_interpreter(max_arena_size, external_memory_size)
         currentModel = None
 
         for model in self.models:

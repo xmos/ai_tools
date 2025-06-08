d8816a284f7b57c7342293c4e63f7cf1663ee355
panickal-xmos <deepakpanickal@xmos.com>
2025-06-08 20:31:47 +0100

    Update version

--- xformer/Version.h
+++ xformer/Version.h
@@ -7,8 +7,8 @@
 namespace xformer {
 
 unsigned majorVersion = 1;
-unsigned minorVersion = 3;
-unsigned patchVersion = 1;
+unsigned minorVersion = 4;
+unsigned patchVersion = 0;
 
 } // namespace xformer
 
